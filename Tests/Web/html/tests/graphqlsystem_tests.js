7b6ff8065c831e737ac94203f3d410e6d36966d5
Rich Searle <richard.searle@magnopus.com>
2023-06-29 11:51:41 +0100

    [OF-693] fix: Revert formatting on js test

--- Tests/Web/html/tests/graphqlsystem_tests.js
+++ Tests/Web/html/tests/graphqlsystem_tests.js
@@ -1,58 +1,54 @@
-import { test, assert } from "../test_framework.js";
-import { generateUniqueString } from "../test_helpers.js";
-import { logIn } from "./usersystem_tests_helpers.js";
-import { createSpace } from "./spacesystem_tests_helpers.js";
+import { test, assert } from '../test_framework.js';
+import { generateUniqueString } from '../test_helpers.js';
+import { logIn } from './usersystem_tests_helpers.js';
+import { createSpace } from './spacesystem_tests_helpers.js';
 
-import { Systems } from "../olympus_foundation.js";
-
-test("GraphQLSystemTests", "QueryTest", async function () {
-  const systemsManager = Systems.SystemsManager.get();
-  const userSystem = systemsManager.getUserSystem();
-  const spaceSystem = systemsManager.getSpaceSystem();
-  const GraphQLSystem = systemsManager.getGraphQLSystem();
-
-  // Generate space name
-  const spaceName = generateUniqueString("OLY-TESTS-WASM-SPACE");
-  const spaceDescription = "OLY-TESTS-WASM-SPACEDESC";
-
-  // Log in
-  await logIn(userSystem);
-
-  // Create space
-  const space = await createSpace(
-    spaceSystem,
-    spaceName,
-    spaceDescription,
-    Systems.SpaceAttributes.Private
-  );
-
-  const query = "spaces{items{name}}";
-
-  // Run GraphQL Query
-  const result = await GraphQLSystem.runQuery(query);
-
-  // Check response contain generated space name
-  const response = result.getResponse();
-  result.delete();
-
-  assert.isTrue(response.includes(spaceName));
-
-  const fullQuery = {
-    query: `query getSpaceNameById($spaceId:String!)  {
-              spaceDetails(groupId: $spaceId) {
-                    name
-                  }
-                }`,
-    variables: {
-      spaceId: space.id,
-    },
-    operationName: "getSpaceNameById",
-  };
+import { Systems } from '../olympus_foundation.js';
 
 
-  const fqResult = await GraphQLSystem.runQuery(JSON.stringify(fullQuery));
-  const fqResponse = fqResult.getResponse();
-  fqResult.delete();
+test('GraphQLSystemTests', 'QueryTest', async function() {
+    const systemsManager = Systems.SystemsManager.get();
+    const userSystem = systemsManager.getUserSystem();
+    const spaceSystem = systemsManager.getSpaceSystem();
+    const GraphQLSystem = systemsManager.getGraphQLSystem()
 
-  assert.isTrue(fqResponse.includes(spaceName));
+    // Generate space name
+    const spaceName = generateUniqueString('OLY-TESTS-WASM-SPACE');
+    const spaceDescription = 'OLY-TESTS-WASM-SPACEDESC';
+
+    // Log in
+    await logIn(userSystem);
+
+    // Create space
+    const space = await createSpace(spaceSystem, spaceName, spaceDescription, Systems.SpaceAttributes.Private);
+
+    const query = "spaces{items{name}}";
+
+    // Run GraphQL Query
+    const result = await GraphQLSystem.runQuery(query);
+
+    // Check response contain generated space name
+    const response = result.getResponse();
+    result.delete();
+
+    assert.isTrue(response.includes(spaceName));
+
+    const fullQuery = {
+        query: `query getSpaceNameById($spaceId:String!)  {
+                  spaceDetails(groupId: $spaceId) {
+                        name
+                      }
+                    }`,
+        variables: {
+          spaceId: space.id,
+        },
+        operationName: "getSpaceNameById",
+      };
+    
+    
+      const fqResult = await GraphQLSystem.runFullQuery(JSON.stringify(fullQuery));
+      const fqResponse = fqResult.getResponse();
+      fqResult.delete();
+    
+      assert.isTrue(fqResponse.includes(spaceName)); 
 });
