63a704b683385a8756056a1777491a12a7f36908
Rich Searle <101268770+MAG-RichardSearle@users.noreply.github.com>
2023-06-30 15:34:04 +0100

    [OF-693] test: c# test for graphQL funFullQuery

--- Tests/CSharp/src/Tests/GraphQLSystemTests.cs
+++ Tests/CSharp/src/Tests/GraphQLSystemTests.cs
@@ -38,6 +38,16 @@ namespace CSPEngine
 
             // Check Response Contains Space Name
             Assert.IsTrue(test.Contains(testSpaceName));
+
+            //Test our full query endpoint.
+            var testFullQuery = "{\"query\":\"\n\nquery getSpaces($limit:Int!)  {\n  spaces(pagination: {limit:$limit}) {\n    items {\n      name\n    }\n  }\n}\n\n\",\"variables\":{\"limit\":5},\"operationName\":\"getSpaces\"}";
+
+            using var resultFQ = graphQLSystem.RunFullQuery(testFullQuery).Result;
+            var testFQ = resultFQ.GetResponse().ToString();
+
+            // Check Response Contains Space Name
+            Assert.IsTrue(testFQ.Contains(testSpaceName));
+
         }
 #endif
     }
