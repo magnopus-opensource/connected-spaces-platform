a8df101a52357e32572929fac53137b75bdf91d7
Sam Birley <sam.birley@magnopus.com>
2023-07-05 14:10:37 +0100

    [OB-2722] test: datetime test coverage
    Additional testing of UTC string parsing and a separate
    test validating the `DateTime` comparison operator.

--- Tests/src/PublicAPITests/DateTimeTests.cpp
+++ Tests/src/PublicAPITests/DateTimeTests.cpp
@@ -18,22 +18,57 @@
 #include "TestHelpers.h"
 
 #include "gtest/gtest.h"
+#include <chrono>
 
-#if RUN_ALL_UNIT_TESTS || RUN_DATE_TIME_TESTS || 1
+#if RUN_ALL_UNIT_TESTS || RUN_DATE_TIME_TESTS || RUN_DATE_TIME_STRING_CONVERSION_TESTS
 CSP_PUBLIC_TEST(CSPEngine, DateTimeTests, UTCStringConversion)
 {
-	const csp::common::String UTCString("2122-04-30T02:00:00+0000");
-	const csp::common::DateTime Date(UTCString);
+	{
+		const csp::common::String UTCString("1999-06-12T08:24:21+00:00");
+		const csp::common::DateTime Date(UTCString);
 
-	auto TimePoint				 = Date.GetTimePoint();
-	auto Time					 = std::chrono::system_clock::to_time_t(TimePoint);
-	const std::tm* const UTCTime = std::gmtime(&Time);
+		auto TimePoint				 = Date.GetTimePoint();
+		auto Time					 = std::chrono::system_clock::to_time_t(TimePoint);
+		const std::tm* const UTCTime = std::gmtime(&Time);
 
-	EXPECT_EQ(UTCTime->tm_year, 2122 - 1900); // tm_year is years since 1900
-	EXPECT_EQ(UTCTime->tm_mon, 3);			  // tm_mon is zero-indexed
-	EXPECT_EQ(UTCTime->tm_mday, 30);
-	EXPECT_EQ(UTCTime->tm_hour, 2);
-	EXPECT_EQ(UTCTime->tm_min, 0);
-	EXPECT_EQ(UTCTime->tm_sec, 0);
+		EXPECT_EQ(UTCTime->tm_year, 1999 - 1900);
+		EXPECT_EQ(UTCTime->tm_mon, 5);
+		EXPECT_EQ(UTCTime->tm_mday, 12);
+		EXPECT_EQ(UTCTime->tm_hour, 8);
+		EXPECT_EQ(UTCTime->tm_min, 24);
+		EXPECT_EQ(UTCTime->tm_sec, 21);
+	}
+
+	{
+		// this UTC date is out of the int32-representable range since the 1980 epoch.
+		// We expect this to pass, as we expect our code to work with 64bit date/time representations.
+		const csp::common::String UTCString("2122-04-30T02:30:54+00:00");
+		const csp::common::DateTime Date(UTCString);
+
+		auto TimePoint				 = Date.GetTimePoint();
+		auto Time					 = std::chrono::system_clock::to_time_t(TimePoint);
+		const std::tm* const UTCTime = std::gmtime(&Time);
+
+		EXPECT_EQ(UTCTime->tm_year, 2122 - 1900); // tm_year is years since 1900
+		EXPECT_EQ(UTCTime->tm_mon, 3);			  // tm_mon is zero-indexed
+		EXPECT_EQ(UTCTime->tm_mday, 30);
+		EXPECT_EQ(UTCTime->tm_hour, 2);
+		EXPECT_EQ(UTCTime->tm_min, 30);
+		EXPECT_EQ(UTCTime->tm_sec, 54);
+	}
+}
+#endif
+
+#if RUN_ALL_UNIT_TESTS || RUN_DATE_TIME_TESTS || RUN_DATE_TIME_COMPARSION
+CSP_PUBLIC_TEST(CSPEngine, DateTimeTests, Comparison)
+{
+	using namespace std::chrono_literals;
+
+	const csp::common::DateTime TimeNow					   = csp::common::DateTime::UtcTimeNow();
+	const std::chrono::system_clock::time_point TimeFuture = TimeNow.GetTimePoint() + std::chrono::system_clock::duration(5min);
+
+	const csp::common::DateTime CurrentDateTime(TimeNow);
+	const csp::common::DateTime FutureDateTime(TimeFuture);
+	ASSERT_GE(FutureDateTime, CurrentDateTime);
 }
 #endif
