e7149207863778863d6b742e8b539643ff8104ac
Michael K <99647234+MAG-MichaelK@users.noreply.github.com>
2023-10-20 09:52:16 +0100

    [OF-815] test: (wrapper gen) unit test async functions
    * [NT-0] refac: formatting changes in unit test gen
    
    Formats Python files using Black and JS/JSON using Prettier-ESLint.
    
    * [OF-815] test: wrapper gen csp_async_result tests

--- UnitTesting/scripts/GenerateExports.py
+++ UnitTesting/scripts/GenerateExports.py
@@ -14,31 +14,28 @@ def main():
     os.chdir(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
     # Clean previously-generated code
-    shutil.rmtree('src', ignore_errors=True)
-    shutil.rmtree('include', ignore_errors=True)
+    shutil.rmtree("src", ignore_errors=True)
+    shutil.rmtree("include", ignore_errors=True)
 
-    os.mkdir('src')
-    os.mkdir('include')
+    os.mkdir("src")
+    os.mkdir("include")
 
     # Initialise Jinja environment
-    env = Environment(
-        loader=FileSystemLoader('templates/exports'),
-        extensions=['jinja2_workarounds.MultiLineInclude']
-    )
+    env = Environment(loader=FileSystemLoader("templates/exports"), extensions=["jinja2_workarounds.MultiLineInclude"])
 
     # Render exports
-    header_template = env.get_template('exports.h.jinja2')
-    source_template = env.get_template('exports.cpp.jinja2')
+    header_template = env.get_template("exports.h.jinja2")
+    source_template = env.get_template("exports.cpp.jinja2")
 
-    with open('include/generated_exports.h', 'w') as f:
+    with open("include/generated_exports.h", "w") as f:
         f.write(header_template.render(config=config))
 
-    with open('src/generated_exports.cpp', 'w') as f:
+    with open("src/generated_exports.cpp", "w") as f:
         f.write(source_template.render(config=config))
 
-    subprocess.run(f"\"clang-format\" -i \"include/generated_exports.h\"", shell=True)
-    subprocess.run(f"\"clang-format\" -i \"src/generated_exports.cpp\"", shell=True)
+    subprocess.run(f'"clang-format" -i "include/generated_exports.h"', shell=True)
+    subprocess.run(f'"clang-format" -i "src/generated_exports.cpp"', shell=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
