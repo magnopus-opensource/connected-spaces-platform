3d34fc9db2926e176ffc35dda05a9143734232c6
MAG-AW <99488940+MAG-AW@users.noreply.github.com>
2023-10-20 17:54:02 +0100

    [NT-0] fix: test failing on InviteToSpace

--- Tests/src/PublicAPITests/SpaceSystemTests.cpp
+++ Tests/src/PublicAPITests/SpaceSystemTests.cpp
@@ -1346,7 +1346,7 @@ CSP_PUBLIC_TEST(CSPEngine, SpaceSystemTests, SetUserRoleOnInviteTest)
 	CreateSpace(SpaceSystem, UniqueSpaceName, TestSpaceDescription, SpaceAttributes::Private, nullptr, nullptr, nullptr, Space);
 
 	// Invite second test account as a Moderator Role user
-	auto [Result] = AWAIT_PRE(SpaceSystem, InviteToSpace, RequestPredicate, Space.Id, AlternativeLoginEmail, true, "https://www.magnopus.com/");
+	auto [Result] = AWAIT_PRE(SpaceSystem, InviteToSpace, RequestPredicate, Space.Id, AlternativeLoginEmail, true, "");
 	EXPECT_EQ(Result.GetResultCode(), csp::services::EResultCode::Success);
 
 	::UserRoleInfo UserRoleInfo;
