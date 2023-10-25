8dedfdef40016fd6ccb43d23f631d9f8d25c1be6
Mag-JB <107475307+Mag-JB@users.noreply.github.com>
2023-10-25 12:24:36 +0100

    [OB-2924] fix: Add support for xcode 14.3+

--- generate_solution_ios.sh
+++ generate_solution_ios.sh
@@ -4,6 +4,8 @@
 git config core.hooksPath .githooks
 git config commit.template .githooks/commit-template.txt
 
+XCODE_VERSION=$(softwareupdate --history | awk '/Command Line Tools for Xcode/ {print $6}' | tail -1)
+
 if [ ! -f /modules/premake/README.md ]
 then
 	git submodule update --recursive
@@ -12,9 +14,18 @@ fi
 
 if [ ! -f /modules/premake/bin/release/premake5 ]
 then
-cd modules/premake/
-make -f Bootstrap.mak macosx
-cd ../..
+	if [ $((${XCODE_VERSION//.})) > 142 ]; then
+		echo download binary
+		cd ./modules/premake/bin/release/
+		wget https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-macosx.tar.gz
+		tar -xzvf premake-5.0.0-beta2-macosx.tar.gz
+		rm premake-5.0.0-beta2-macosx.tar.gz
+		cd ../../../..
+	else
+		cd ./modules/premake/
+		make -f Bootstrap.mak macosx
+		cd ../..
+	fi
 fi
 
 ./modules/premake/bin/release/premake5 --os=ios xcode4 "$@"
