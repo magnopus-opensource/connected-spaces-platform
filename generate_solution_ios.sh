4783de1bb084567786a76d6301a50b37be9f919b
Sam Birley <sam.birley@magnopus.com>
2024-03-04 17:02:03 +0000

    [NT-0] fix: removing legacy premake install path
    The legacy implementation for installing premake is no longer required. Removing!

--- generate_solution_ios.sh
+++ generate_solution_ios.sh
@@ -16,19 +16,12 @@ fi
 
 if [ ! -f /modules/premake/bin/release/premake5 ]
 then
-	MIN_XCODE_VERSION="14.2"
-    if [ "$(printf '%s\n' "$MIN_XCODE_VERSION" "$XCODE_VERSION" | sort -V | head -n1)" = "$MIN_XCODE_VERSION" ]; then
-		mkdir -p ./modules/premake/bin/release/
-		cd ./modules/premake/bin/release/
-		curl -O -L https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-macosx.tar.gz
-		tar -xzvf premake-5.0.0-beta2-macosx.tar.gz
-		rm premake-5.0.0-beta2-macosx.tar.gz
-		cd ../../../..
-	else
-		cd ./modules/premake/
-		make -f Bootstrap.mak macosx
-		cd ../..
-	fi
+    mkdir -p ./modules/premake/bin/release/
+    cd ./modules/premake/bin/release/
+    curl -O -L https://github.com/premake/premake-core/releases/download/v5.0.0-beta2/premake-5.0.0-beta2-macosx.tar.gz
+    tar -xzvf premake-5.0.0-beta2-macosx.tar.gz
+    rm premake-5.0.0-beta2-macosx.tar.gz
+    cd ../../../..
 fi
 
 ./modules/premake/bin/release/premake5 --os=ios xcode4 "$@"
