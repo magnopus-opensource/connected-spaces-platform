0d9f99ebb923b4a4f72a9dab7a8e2e6c6da80b29
mag-richardzirbes <100873114+mag-richardzirbes@users.noreply.github.com>
2024-12-10 06:52:13 -0800

    Asset: make ThirdParty properties public, remove methods (#561)
    * Asset: make ThirdParty properties public, remove methods
    
    * Update tests to remove method for Asset ThirdParty*
    
    * Update tests to remove method for Asset ThirdParty*
    
    * Fix misspelling in test replaced code
    
    * Use variables to store identifier string in tests

--- Library/src/Systems/Assets/AssetSystem.cpp
+++ Library/src/Systems/Assets/AssetSystem.cpp
@@ -519,7 +519,7 @@ void AssetSystem::UpdateAsset(const Asset& Asset, AssetResultCallback Callback)
 	}
 
 	AssetInfo->SetAddressableId(
-		StringFormat("%s|%d", Asset.GetThirdPartyPackagedAssetIdentifier().c_str(), static_cast<int>(Asset.GetThirdPartyPlatformType())));
+		StringFormat("%s|%d", Asset.ThirdPartyPackagedAssetIdentifier.c_str(), static_cast<int>(Asset.ThirdPartyPlatformType)));
 
 	AssetInfo->SetAssetType(ConvertAssetTypeToString(Asset.Type));
 	services::ResponseHandlerPtr ResponseHandler
