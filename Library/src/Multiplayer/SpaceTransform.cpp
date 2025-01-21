04e69673f6fb25f66e223e114b7dfdf4b63f6c3a
Elliot Morris <Elliot.Morris@magnopus.com>
2025-01-21 16:54:32 +0000

    [OF-1494] fix: Fix SpaceTransform multiplication (#594)
    * [OF-1494] fix: Fix SpaceTransform multiplication
    
    The existing implementation had several things wrong with it, one has to imagine
    it was never called anywhere, because this would be so apparently.
    Unfortunately, it was untested since the beginning.
    
    Issues :
    - [[nodiscard]] warning on normalize ignored, although post-rotation normalization is only needed to prevent cascading floating point errors
    - The W component for glm::quat is the first argument, but for our vectors is is the last, all our quaternions were being constructed with nonsense values. (Strong typing please! Vector4 is inappropriate to store quaternions for precisely this reason)
    - I don't think you can do complex compositions in this manner, separating the translation, scale and rotation components like that.
    - Untested
    
    The prior code, even with the code issues fixed, failed to pass even the identity assumptions in the new test suite that has been written.
    For that reason, and due to the face that matrix compositions are more comprehensible, I've replaced the implementation with a matrix
    based one. It's more verbose to be sure, and probably slightly slower than direct quaternion work, but at least it has a chance of being correct.
    
    * fixup! [OF-1494] fix: Fix SpaceTransform multiplication
    
    Unnecesary includes
    
    * fixup! [OF-1494] fix: Fix SpaceTransform multiplication
    
    Identity no result
    
    * fixup! [OF-1494] fix: Fix SpaceTransform multiplication
    
    equivalent typo
    
    * fixup! [OF-1494] fix: Fix SpaceTransform multiplication
    
    radians direct
    
    * fixup! [OF-1494] fix: Fix SpaceTransform multiplication
    
    named 90deg constant

--- Library/src/Multiplayer/SpaceTransform.cpp
+++ Library/src/Multiplayer/SpaceTransform.cpp
@@ -16,7 +16,11 @@
 
 #include "CSP/Multiplayer/SpaceTransform.h"
 
+#define GLM_ENABLE_EXPERIMENTAL // glm::decompose is still technically an experimental feature, but better than a hand-rolled solution in my opinion.
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/matrix_decompose.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <tuple>
 
 csp::multiplayer::SpaceTransform::SpaceTransform()
     : Position(0.0f, 0.0f, 0.0f)
@@ -38,13 +42,53 @@ bool csp::multiplayer::SpaceTransform::operator==(const SpaceTransform& Transfor
     return Position == Transform.Position && Rotation == Transform.Rotation && Scale == Transform.Scale;
 }
 
+bool csp::multiplayer::SpaceTransform::operator!=(const SpaceTransform& Transform) const { return !(*this == Transform); }
+
+namespace
+{
+/*
+ * Return the TRS components derived from the position, quaternion and scale values set in this transform.
+ * First is T(Translation), Second is R(Rotation), Third is S(Scale)
+ * All Mat4s for uniform application, despite slight space redundancy in T and S matrices.
+ */
+std::tuple<glm::mat4, glm::mat4, glm::mat4> GetTRSComponents(const csp::multiplayer::SpaceTransform& Transform)
+{
+    glm::mat4 TMatrix = glm::mat4(1.0f); // mat4(1.0f) gives an identity matrix
+    TMatrix = glm::translate(TMatrix, glm::vec3 { Transform.Position.X, Transform.Position.Y, Transform.Position.Z });
+
+    // Quaternions must be normalized in order to not introduce crazy chaos.
+    glm::quat NormalQ = glm::normalize(glm::quat(Transform.Rotation.W, Transform.Rotation.X, Transform.Rotation.Y, Transform.Rotation.Z));
+    glm::mat4 RMatrix = glm::toMat4(std::move(NormalQ));
+
+    glm::mat4 SMatrix = glm::mat4(1.0f);
+    SMatrix = glm::scale(SMatrix, glm::vec3(Transform.Scale.X, Transform.Scale.Y, Transform.Scale.Z));
+
+    return { std::move(TMatrix), std::move(RMatrix), std::move(SMatrix) };
+}
+}
+
 csp::multiplayer::SpaceTransform csp::multiplayer::SpaceTransform::operator*(const SpaceTransform& Transform) const
 {
-    glm::quat Orientation { Rotation.X, Rotation.Y, Rotation.Z, Rotation.W };
-    glm::quat OtherOrientation { Transform.Rotation.X, Transform.Rotation.Y, Transform.Rotation.Z, Transform.Rotation.W };
-    glm::quat FinalOrientation = OtherOrientation * Orientation;
-    // Temporarily supress the unused variable warning
-    [[maybe_unused]] auto temporary = glm::normalize(FinalOrientation);
+    // Transformation order is important when applying TRS matrices. Scale first, then Rotation, then Translation.
+    // Otherwise, non-uniform scaling is effected by previous rotation, introducing skew, which is not normally desired.
+    const auto [T1, R1, S1] = GetTRSComponents(*this);
+    const auto [T2, R2, S2] = GetTRSComponents(Transform);
+
+    const auto TRS1 = T1 * R1 * S1; // Remember, "First" in matrix composition means the last element listed in the multiplication
+    const auto TRS2 = T2 * R2 * S2;
+    const auto ComposedTRS = TRS1 * TRS2;
+
+    glm::vec3 Scale;
+    glm::quat Rotation;
+    glm::vec3 Translation;
+    glm::vec3 Skew;
+    glm::vec4 Perspective;
+    glm::decompose(ComposedTRS, Scale, Rotation, Translation, Skew, Perspective);
+    // We're ignoring skew and perspective, as TRS composition should not have introduced any,
+    // provided inputs are normalized.
+    // Note, if you're getting confusing outputs for rotation when you have negative scales, you're
+    // probably flipping the rotation, which is _fine_, but hard to understand without visualization.
+
     return SpaceTransform(
-        Position + Transform.Position, { FinalOrientation.x, FinalOrientation.y, FinalOrientation.z, FinalOrientation.w }, Scale * Transform.Scale);
+        { Translation.x, Translation.y, Translation.z }, { Rotation.x, Rotation.y, Rotation.z, Rotation.w }, { Scale.x, Scale.y, Scale.z });
 }
