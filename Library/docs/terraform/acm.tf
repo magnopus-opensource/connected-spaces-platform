d72f947fc810fb7bfe9bd928c236c7b4a081663e
Ray Saltrelli <ray.saltrelli@magnopus.com>
2025-01-21 16:13:27 -0500

    Use appropriate cert

--- Library/docs/terraform/acm.tf
+++ Library/docs/terraform/acm.tf
@@ -1,4 +1,4 @@
 data "aws_acm_certificate" "main" {
-  domain   = data.aws_route53_zone.alias.name
+  domain   = var.alias_aws_public_zone_id != null ? data.aws_route53_zone.alias.name : data.aws_route53_zone.canonical.name
   statuses = ["ISSUED"]
 }
