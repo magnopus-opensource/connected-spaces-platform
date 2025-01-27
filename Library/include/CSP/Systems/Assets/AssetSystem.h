53958ee9eeda337496594287e0510a6e6732d898
MAG-mv <109593178+MAG-mv@users.noreply.github.com>
2025-01-27 10:17:59 +0000

    [OF-1385] doc: update comments

--- Library/include/CSP/Systems/Assets/AssetSystem.h
+++ Library/include/CSP/Systems/Assets/AssetSystem.h
@@ -158,9 +158,9 @@ public:
 
 	/// @brief Retrieves the number asset collections based on the specified search criteria.
 	/// @param ParentId csp::common::String : optional asset collection parent id to get asset collections associated with it
-	/// @param Names csp::common::Optional<csp::common::Array<csp::common::String>> : optional array of strings representing asset
+	/// @param Names csp::common::Optional<csp::common::Array<csp::common::String>> : optional array of asset collection names
 	/// @param Types EAssetCollectionType : type of the asset collection
-	/// @param Tags csp::common::Array<csp::common::String> : optional array of strings representing asset collection tags
+	/// @param Tags csp::common::Array<csp::common::String> : optional array of asset collection tags
 	/// collection names
 	/// @param SpaceIds csp::common::Array<csp::common::String> : optional space ids to get asset collections associated with them
 	/// @param Callback AssetCollectionCountResultCallback : callback when asynchronous task finishes
