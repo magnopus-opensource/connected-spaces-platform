b779ae5d6d567ba86f9ce0ebe35524191021abc4
MAG-mv <109593178+MAG-mv@users.noreply.github.com>
2025-01-30 17:12:39 +0000

    [OF-1353] fix: add wait to ensure component data is written to the database by chs

--- Tests/src/PublicAPITests/ConversationSystemTests.cpp
+++ Tests/src/PublicAPITests/ConversationSystemTests.cpp
@@ -552,6 +552,9 @@ CSP_PUBLIC_TEST(CSPEngine, ConversationSystemTests, TwoConversationsTest)
 
     LogOut(UserSystem);
 
+    // Ensure component data has been written to database by chs before entering the space again
+    std::this_thread::sleep_for(3s);
+
     // Log in with the second account
     csp::common::String SecondTestUserId;
     LogIn(UserSystem, SecondTestUserId, AlternativeTestUser.Email, GeneratedTestAccountPassword);
