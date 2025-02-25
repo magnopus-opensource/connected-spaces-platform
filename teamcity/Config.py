80602377694fedc0f6cf0d02c1ec613e0f3e7631
MAG-AdamThorn <adam.thorn@magnopus.com>
2025-02-25 15:51:11 +0000

    [OF-1580] ci: Add DebugStatic build to Unity package

--- teamcity/Config.py
+++ teamcity/Config.py
@@ -59,6 +59,7 @@ config = Config(
         ],
         ios=[
             "libConnectedSpacesPlatform.a",
+            "libConnectedSpacesPlatform_D.zip",
             "libcrypto.a",
             "libssl.a"
         ],
