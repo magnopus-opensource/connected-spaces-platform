a686212dd668663d2253abce23d53bca43a45339
Adrian Meredith <adrian.meredith@magnopus.com>
2025-03-14 12:03:09 +0000

    fixed memory error

--- Library/include/CSP/Systems/Assets/GLTFMaterial.h
+++ Library/include/CSP/Systems/Assets/GLTFMaterial.h
@@ -177,6 +177,8 @@ public:
     /// @param AssetId const csp::common::String& : The asset where the material info is stored
     GLTFMaterial(const csp::common::String& Name, const csp::common::String& AssetCollectionId, const csp::common::String& AssetId);
 
+    virtual ~GLTFMaterial() = default;
+
     GLTFMaterial();
 
 private:
