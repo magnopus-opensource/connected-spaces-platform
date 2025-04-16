aa7acdec9fa8d7b22245fed4b6b3cdb631e9e643
MAG-OR <185054971+MAG-OR@users.noreply.github.com>
2025-04-16 12:41:58 +0100

    [OF-1550] fix: put -Wextra first in the hope that position matters and
    that the missing field initializer errors will now actually be ignored
    from the macos build

--- Library/premake5.lua
+++ Library/premake5.lua
@@ -173,7 +173,7 @@ if not Project then
 			buildoptions {
 				"-Wno-error=deprecated-declarations", --Don't error on deprecation warnings, this is because we use Uri a lot in our services generated code, which has deprecation warnings for some unused but still generated endpoints.
 				"-Wno-braced-scalar-init", -- Don't warn against doing stuff like `return {0}`, which we do in the interop output.
-                                "-Wno-missing-field-initializers", -- todo comment 
+                                "-Wno-missing-field-initializers", -- Don't warn against missing field initializers (because of the wrapper generator)
 				"-Wno-error=unused-lambda-capture", --This shouldn't be disabled, we just had to rush to unblock android builds. Take all the this captures out and remove.
 				"-Wno-unknown-pragmas", --Also not the greatest. This is to try and suppress a signalR warning, even though the signalR project dosen't emit warnings (I think this error is a bit unique cause of preprocessor stuff)
 				"-Wno-error=nonportable-include-path", --Include paths dont match file structure. Should get around to fixing
@@ -216,7 +216,7 @@ if not Project then
 			-- Could not manage to get xcode to co-operate in any other less specific manner of setting the flags.
 			-- These disables are to do with warnings in generated code that we should get around to dealing with.
 			xcodebuildsettings {
-				["WARNING_CFLAGS"] = "-Wno-error=deprecated-declarations -Wno-braced-scalar-init -Wno-missing-field-initializers -Wextra"
+				["WARNING_CFLAGS"] = "-Wextra -Wno-error=deprecated-declarations -Wno-braced-scalar-init -Wno-missing-field-initializers"
 			}
 
             links { 
@@ -237,7 +237,7 @@ if not Project then
 			-- Could not manage to get xcode to co-operate in any other less specific manner of setting the flags.
 			-- These disables are to do with warnings in generated code that we should get around to dealing with.
 			xcodebuildsettings {
-				["WARNING_CFLAGS"] = "-Wno-error=deprecated-declarations -Wno-braced-scalar-init -Wno-missing-field-initializers -Wextra"
+				["WARNING_CFLAGS"] = "-Wextra -Wno-error=deprecated-declarations -Wno-braced-scalar-init -Wno-missing-field-initializers"
 			}
 
             links {
@@ -255,10 +255,10 @@ if not Project then
             buildoptions {
                 "--no-entry",           -- remove default library entry point
                 "-pthread",             -- enable threading
-                "-fwasm-exceptions",     -- enable native wasm exceptions
-				"-Wno-error=deprecated-declarations", --Don't error on deprecation warnings, this is because we use Uri a lot in our services generated code, which has deprecation warnings for some unused but still generated endpoints.
-				"-Wno-braced-scalar-init", -- Don't warn against doing stuff like `return {0}`, which we do in the interop output.
-                                "-Wno-missing-field-initializers" -- todo comment
+                "-fwasm-exceptions",    -- enable native wasm exceptions
+	        "-Wno-error=deprecated-declarations", --Don't error on deprecation warnings, this is because we use Uri a lot in our services generated code, which has deprecation warnings for some unused but still generated endpoints.
+		"-Wno-braced-scalar-init", -- Don't warn against doing stuff like `return {0}`, which we do in the interop output.
+                "-Wno-missing-field-initializers" -- Don't warn against missing field initializers (because of the wrapper generator)
             }
 
             linkoptions { 
