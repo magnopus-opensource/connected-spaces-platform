1671fde35e3dc6dabd13c059a2ca86370a73f25a
MAG-mv <109593178+MAG-mv@users.noreply.github.com>
2025-05-06 11:53:19 +0100

    [OF-1510] fix: handling case where variant is invalid

--- Library/src/Multiplayer/SignalRSerializer.cpp
+++ Library/src/Multiplayer/SignalRSerializer.cpp
@@ -56,9 +56,14 @@ signalr::value SignalRSerializer::Get() const
         throw std::runtime_error("Invalid call: Serializer is not at the root");
     }
 
+    if (Stack.top().valueless_by_exception())
+    {
+        throw std::runtime_error("Invalid state");
+    }
+
     signalr::value SerializedValue;
     // Dispatch internal variant type to the correct GetInternal call
-    std::visit([this, &SerializedValue](const auto& ValType) mutable { SerializedValue = this->GetInternal(ValType); }, Stack.top());
+    std::visit([this, &SerializedValue](const auto& ValType) { SerializedValue = this->GetInternal(ValType); }, Stack.top());
 
     return SerializedValue;
 }
