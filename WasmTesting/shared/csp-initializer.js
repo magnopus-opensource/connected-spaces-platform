c3cdd3897c2363f9e95b65b36446cd4c090d292a
Elliot Morris <Elliot.Morris@magnopus.com>
2025-05-22 13:22:09 +0100

    [OF-1653] test: Add shared library initialization
    Encapsulate the library initialization into a shared module.
    
    The reason this is done is because we will want to use CSP in the uvu (typescript) tests in order to validate expectations, do pre-work prior to puppeteer invocation, or even just to directly do tests there when browser involvement is more headache than it's worth.
    
    We also need to initialize CSP in the browser launched by puppeteer, so this prevents us duplicating that logic, and ensures we're on the same tenant and such.

---
+++ WasmTesting/shared/csp-initializer.js
@@ -0,0 +1,37 @@
+import { ready, CSPFoundation, ClientUserAgent} from '../node_modules/connected-spaces-platform.web/connectedspacesplatform.js';
+
+let CHS_ENDPOINT_ROOT = "https://ogs-internal.magnopus-dev.cloud";
+let TENANT = "OKO_TESTS";
+let TESTS_CLIENT_SKU = "WASMTest";
+let TESTS_CLIENT_OS = "WASMTestsOS";
+
+export async function initializeCSP(useDebugCSP) {
+  try {
+    console.log(
+      useDebugCSP
+        ? "Initializing with Debug CSP"
+        : "Initializing with Release CSP"
+    );
+
+    await ready(useDebugCSP);
+
+    CSPFoundation.initialise(CHS_ENDPOINT_ROOT, TENANT);
+    if (!CSPFoundation.getIsInitialised()) {
+      throw new Error("CSPFoundation failed to initialize");
+    }
+
+    const userAgent = ClientUserAgent.create();
+    userAgent.cSPVersion = CSPFoundation.getBuildID();
+    userAgent.clientOS = TESTS_CLIENT_OS;
+    userAgent.clientSKU = TESTS_CLIENT_SKU;
+    userAgent.clientVersion = CSPFoundation.getBuildID();
+    userAgent.clientEnvironment = "ODev";
+    userAgent.cHSEnvironment = "oDev";
+
+    CSPFoundation.setClientUserAgentInfo(userAgent);
+    console.log("CSP Initialization complete.");
+  } catch (error) {
+    console.error("Failed to initialize CSP:", error.message);
+    console.error(error.stack);
+  }
+}
