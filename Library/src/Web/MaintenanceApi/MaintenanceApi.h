7ed7651950e72456338dfd45cb8ec9e151c93157
MAG-mv <109593178+MAG-mv@users.noreply.github.com>
2025-06-16 13:00:53 +0100

    [OF-1672] fix: Addin missing web folder

---
+++ Library/src/Web/MaintenanceApi/MaintenanceApi.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 Magnopus LLC
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "CSP/Common/CancellationToken.h"
+#include "Services/ApiBase/ApiBase.h"
+
+namespace csp::services
+{
+
+template <typename T, typename U, typename V, typename W> class ApiResponseHandler;
+
+} // namespace csp::services
+
+namespace csp::web
+{
+
+class WebClient;
+
+}
+
+namespace csp::systems::maintenanceservice
+{
+class MaintenanceApi : public csp::services::ApiBase
+{
+public:
+    MaintenanceApi(csp::web::WebClient* InWebClient);
+    ~MaintenanceApi();
+
+    void Query(const csp::common::String& MaintenanceUrl, csp::services::ApiResponseHandlerBase* ResponseHandler,
+        csp::common::CancellationToken& CancellationToken = csp::common::CancellationToken::Dummy()) const;
+};
+} // namespace csp::systems::maintenanceservice
