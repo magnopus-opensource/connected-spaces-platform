a3ce3db2473a4873ea9408bf7f3b620250b907f0
MAG-mv <109593178+MAG-mv@users.noreply.github.com>
2025-06-27 11:22:24 +0100

    [OF-1691] feat: Added csp scene description

---
+++ Library/src/CSPSceneDescription.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 Magnopus LLC
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "CSP/CSPSceneDescription.h"
+#include "Multiplayer/MCS/MCSSceneDescription.h"
+#include "Multiplayer/MCS/MCSTypes.h"
+#include "Services/AggregationService/Dto.h"
+#include "Services/PrototypeService/Dto.h"
+#include "Services/UserService/Dto.h"
+
+namespace csp
+{
+SceneDescription::SceneDescription(
+    const csp::multiplayer::mcs::SceneDescription& MCSSceneDescription, csp::multiplayer::SpaceEntitySystem& /* EntitySystem*/)
+{
+    csp::systems::GroupDtoToSpace(MCSSceneDescription.Group, Space);
+
+    AssetCollections = csp::common::Array<csp::systems::AssetCollection>(MCSSceneDescription.Prototypes.size());
+
+    size_t PrototypesIndex = 0;
+    for (const auto& Prototype : MCSSceneDescription.Prototypes)
+    {
+        csp::systems::PrototypeDtoToAssetCollection(Prototype, AssetCollections[PrototypesIndex]);
+        PrototypesIndex++;
+    }
+
+    size_t AssetDetailIndex = 0;
+    for (const auto& AssetDetail : MCSSceneDescription.AssetDetails)
+    {
+        csp::systems::AssetDetailDtoToAsset(AssetDetail, Assets[AssetDetailIndex]);
+        AssetDetailIndex++;
+    }
+
+    size_t SequenceIndex = 0;
+    for (const auto& Sequence : MCSSceneDescription.Sequences)
+    {
+        csp::systems::SequenceDtoToSequence(Sequence, Sequences[AssetDetailIndex]);
+        SequenceIndex++;
+    }
+}
+}
