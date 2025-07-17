6e8b291f43adb1108226af9fb1e8077755ed8b2d
MAG-ChristopherAtkinson <Christopher.Atkinson@magnopus.global>
2025-07-17 14:32:47 +0100

    [OF-1710] docs: removed TODO comment in inline documentation

--- Library/src/Systems/Settings/ApplicationSettingsSystem.cpp
+++ Library/src/Systems/Settings/ApplicationSettingsSystem.cpp
@@ -139,7 +139,6 @@ async::task<ApplicationSettingsResult> ApplicationSettingsSystem::GetSettingsByC
         = ApplicationSettingsAPI->CreateHandler<ApplicationSettingsResultCallback, ApplicationSettingsResult, void, chs::ApplicationSettingsDto>(
             [](const ApplicationSettingsResult&) {}, nullptr, web::EResponseCodes::ResponseOK, std::move(*OnCompleteEvent.get()));
 
-    // TODO: [OF-1710] include use of 'const csp::common::Optional<csp::common::Array<csp::common::String>>& Keys'
     static_cast<chs::ApplicationSettingsApi*>(ApplicationSettingsAPI)
         ->tenantsTenantApplicationsApplicationNameSettingsContextGet(Tenant, ApplicationName, Context, Convert(Keys), SettingsResponseHandler);
 
