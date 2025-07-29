0816aee329e9c0d2abeacab32f587efff6de6ea7
MAG-ChristopherAtkinson <Christopher.Atkinson@magnopus.global>
2025-07-29 13:45:03 +0100

    [OF-1710] docs: added additional inline documentation to capture elevated premissions in create

--- Library/include/CSP/Systems/Settings/ApplicationSettingsSystem.h
+++ Library/include/CSP/Systems/Settings/ApplicationSettingsSystem.h
@@ -50,6 +50,8 @@ class CSP_API ApplicationSettingsSystem : public SystemBase
 
 public:
     /// @brief Asynchronously creates or updates application settings for a specific context.
+    /// Elevated Permissions Required: This function requires elevated permissions to execute successfully. If the user does not possess any of the
+    /// necessary roles, the operation will failwith a 403 Forbidden error.
     /// @param ApplicationSettings ApplicationSettings& : The settings object containing application name, context, and key-value pairs to be stored.
     /// @param Callback ApplicationSettingsResultCallback : Callback when asynchronous task finishes.
     CSP_NO_EXPORT void CreateSettingsByContext(const ApplicationSettings& ApplicationSettings, ApplicationSettingsResultCallback Callback);
