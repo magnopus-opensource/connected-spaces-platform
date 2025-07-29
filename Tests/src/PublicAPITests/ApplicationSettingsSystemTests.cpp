96ef1ed78e67deaed824bac855ed0c312eba93d3
MAG-ChristopherAtkinson <Christopher.Atkinson@magnopus.global>
2025-07-29 13:45:40 +0100

    [OF-1710] test: added additional test coverage for create

--- Tests/src/PublicAPITests/ApplicationSettingsSystemTests.cpp
+++ Tests/src/PublicAPITests/ApplicationSettingsSystemTests.cpp
@@ -102,6 +102,142 @@ void AttemptToSeedLocalCHSApplicationSettings()
 
 }
 
+CSP_PUBLIC_TEST(CSPEngine, ApplicationSettingsSystemTests, CreateSettingsByContextTest)
+{
+    SetRandSeed();
+
+    auto& SystemsManager = csp::systems::SystemsManager::Get();
+    auto* UserSystem = SystemsManager.GetUserSystem();
+    auto* ApplicationSettingsSystem = SystemsManager.GetApplicationSettingsSystem();
+
+    // Login
+    csp::common::String UserId;
+    LogInAsLocalAdminUser(UserSystem, UserId);
+
+    // In the event that login fails, we do not want to attempt to create the application settings.
+    // It is expected that the login attempt will fail for any deployment that is not the local magnopus services.
+    if (UserId.IsEmpty())
+        return;
+
+    auto ApplicationSettingsTestData = GetApplicationSettingsAnonymousTestData();
+
+    // Create Application Settings
+    {
+        RAIIMockLogger MockLogger {};
+        csp::systems::SystemsManager::Get().GetLogSystem()->SetSystemLevel(LogLevel::Log);
+
+        // Set an expectation that the mock logger will receive message for a successful creation of settings by context
+        const String GetSettingsByContextMsg = "ApplicationSettingsSystem::CreateSettingsByContext successfully created application settings";
+        EXPECT_CALL(MockLogger.MockLogCallback, Call(GetSettingsByContextMsg)).Times(1);
+
+        // Set an expectation that the mock logger will receive message for a successful creation
+        const String SendResultMsg = "Successfully created application settings.";
+        EXPECT_CALL(MockLogger.MockLogCallback, Call(SendResultMsg)).Times(1);
+
+        auto [Result] = AWAIT(ApplicationSettingsSystem, CreateSettingsByContext, ApplicationSettingsTestData);
+
+        EXPECT_EQ(Result.GetResultCode(), csp::systems::EResultCode::Success);
+
+        const auto ApplicationSettings = Result.GetApplicationSettings();
+
+        EXPECT_EQ(ApplicationSettings.AllowAnonymous, ApplicationSettingsTestData.AllowAnonymous);
+        EXPECT_EQ(ApplicationSettings.ApplicationName, ApplicationSettingsTestData.ApplicationName);
+        EXPECT_EQ(ApplicationSettings.Context, ApplicationSettingsTestData.Context);
+        EXPECT_EQ(ApplicationSettings.Settings, ApplicationSettingsTestData.Settings);
+    }
+
+    // Log out
+    LogOut(UserSystem);
+}
+
+CSP_PUBLIC_TEST(CSPEngine, ApplicationSettingsSystemTests, CreateSettingsByContextAnonymousTest)
+{
+    SetRandSeed();
+
+    auto& SystemsManager = csp::systems::SystemsManager::Get();
+    auto* UserSystem = SystemsManager.GetUserSystem();
+    auto* ApplicationSettingsSystem = SystemsManager.GetApplicationSettingsSystem();
+
+    // Login
+    csp::common::String UserId;
+    LogInAsLocalAdminUser(UserSystem, UserId);
+
+    // In the event that login fails, we do not want to attempt to create the application settings.
+    // It is expected that the login attempt will fail for any deployment that is not the local magnopus services.
+    if (UserId.IsEmpty())
+        return;
+
+    auto ApplicationSettingsTestData = GetApplicationSettingsAnonymousTestData();
+
+    // Create Application Settings
+    {
+        RAIIMockLogger MockLogger {};
+        csp::systems::SystemsManager::Get().GetLogSystem()->SetSystemLevel(LogLevel::Log);
+
+        // Set an expectation that the mock logger will receive message for a successful creation of settings by context
+        const String GetSettingsByContextMsg = "ApplicationSettingsSystem::CreateSettingsByContext successfully created application settings";
+        EXPECT_CALL(MockLogger.MockLogCallback, Call(GetSettingsByContextMsg)).Times(1);
+
+        // Set an expectation that the mock logger will receive message for a successful creation
+        const String SendResultMsg = "Successfully created application settings.";
+        EXPECT_CALL(MockLogger.MockLogCallback, Call(SendResultMsg)).Times(1);
+
+        auto [Result] = AWAIT(ApplicationSettingsSystem, CreateSettingsByContext, ApplicationSettingsTestData);
+
+        EXPECT_EQ(Result.GetResultCode(), csp::systems::EResultCode::Success);
+
+        const auto ApplicationSettings = Result.GetApplicationSettings();
+
+        EXPECT_EQ(ApplicationSettings.AllowAnonymous, ApplicationSettingsTestData.AllowAnonymous);
+        EXPECT_EQ(ApplicationSettings.ApplicationName, ApplicationSettingsTestData.ApplicationName);
+        EXPECT_EQ(ApplicationSettings.Context, ApplicationSettingsTestData.Context);
+        EXPECT_EQ(ApplicationSettings.Settings, ApplicationSettingsTestData.Settings);
+    }
+
+    // Log out
+    LogOut(UserSystem);
+}
+
+CSP_PUBLIC_TEST(CSPEngine, ApplicationSettingsSystemTests, CreateInvalidSettingsByContextTest)
+{
+    SetRandSeed();
+
+    auto& SystemsManager = csp::systems::SystemsManager::Get();
+    auto* UserSystem = SystemsManager.GetUserSystem();
+    auto* ApplicationSettingsSystem = SystemsManager.GetApplicationSettingsSystem();
+
+    // Login
+    csp::common::String UserId;
+    LogInAsLocalAdminUser(UserSystem, UserId);
+
+    // In the event that login fails, we do not want to attempt to create the application settings.
+    // It is expected that the login attempt will fail for any deployment that is not the local magnopus services.
+    if (UserId.IsEmpty())
+        return;
+
+    auto ApplicationSettingsTestData = ::ApplicationSettings();
+
+    // Create Application Settings
+    {
+        RAIIMockLogger MockLogger {};
+        csp::systems::SystemsManager::Get().GetLogSystem()->SetSystemLevel(LogLevel::Log);
+
+        // Set an expectation that the mock logger will receive message for a failed result 404 no payload/error message.
+        const String GetRequestErrorMsg = "has returned a failed response (404) but with no payload/error message.";
+        EXPECT_CALL(MockLogger.MockLogCallback, Call(testing::HasSubstr(GetRequestErrorMsg))).Times(1);
+
+        const String ErrorMsg = "Failed to create application settings";
+        EXPECT_CALL(MockLogger.MockLogCallback, Call(ErrorMsg)).Times(1);
+
+        auto [Result] = AWAIT(ApplicationSettingsSystem, CreateSettingsByContext, ApplicationSettingsTestData);
+
+        EXPECT_EQ(Result.GetResultCode(), csp::systems::EResultCode::Failed);
+    }
+
+    // Log out
+    LogOut(UserSystem);
+}
+
 CSP_PUBLIC_TEST(CSPEngine, ApplicationSettingsSystemTests, GetSettingsByContextTest)
 {
     SetRandSeed();
