727b46121f0230af237e4f2c4d4c488ec7b7f0b1
Elliot Morris <Elliot.Morris@magnopus.com>
2025-08-17 18:28:59 +0100

    [OF-1689] tests: Offline Script system tests
    Matrix the script system tests to all run in offline mode.
    
    Notable things :
    - Re-enabled the disabled test here, which I think was caused by a misunderstanding/misnaming of the `EntityCreatedCallback`, it should be `RemoteEntityCreatedCallback` I think.
    - Made mutexes recursive again, thought I could get away with it, I couldn't.
    - Added new public Api because it was useful in testing (FindFirstComponentOfType) and I don't know why it wouldn't be useful externally
    - Had to re-add the OnPropertyChanged immediate thing because of how we do dirty components, want to fix that.
    - Completely rewrote AddSecondScriptTest as it was incomprehensible and was the first test to manifest the issue with the OnPropertyChanged thing, so I needed it understandable so I could debug.

--- Library/src/Multiplayer/ComponentBase.cpp
+++ Library/src/Multiplayer/ComponentBase.cpp
@@ -253,6 +253,12 @@ void ComponentBase::SetProperty(uint32_t Key, const csp::common::ReplicatedValue
         // TODO, fix. Look at `SetPropertyFromPatch` below, it's basically a `SetPropetyDirect`
         Properties[Key] = Value;
         Parent->UpdateComponent(this);
+
+        // MEGA HACK
+        // So, this is for the case where we already have a dirty component of update type ADD pending, but we've just updated it.
+        // It just so happens that the ADD is enough to replicate the component well enough, as anything will do, BUT, we still
+        // need to notify the scripting system here that a property has changed. Absolutely awful, must fix.
+        Parent->OnPropertyChanged(this, Key);
     }
 }
 
