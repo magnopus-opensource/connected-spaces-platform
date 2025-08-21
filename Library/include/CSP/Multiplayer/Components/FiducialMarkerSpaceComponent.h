3c58c7b3bdde2adb19e8208551f21759f421aa26
MAG-ChristopherAtkinson <Christopher.Atkinson@magnopus.global>
2025-08-21 10:11:51 +0100

    [OF-1741] feat: intergrated unresolved 'IVisibleComponent' interface in 'FiducialMarkerSpaceComponent'

--- Library/include/CSP/Multiplayer/Components/FiducialMarkerSpaceComponent.h
+++ Library/include/CSP/Multiplayer/Components/FiducialMarkerSpaceComponent.h
@@ -39,6 +39,7 @@ enum class FiducialMarkerPropertyKeys
     Scale,
     IsVisible,
     IsARVisible,
+    IsVirtualVisible,
     Num
 };
 
@@ -107,6 +108,10 @@ public:
     bool GetIsARVisible() const override;
     /// @copydoc IVisibleComponent::SetIsARVisible()
     void SetIsARVisible(bool InValue) override;
+    /// @copydoc IVisibleComponent::GetIsVirtualVisible()
+    bool GetIsVirtualVisible() const override;
+    /// @copydoc IVisibleComponent::SetIsVirtualVisible()
+    void SetIsVirtualVisible(bool InValue) override;
     /// @}
 };
 
