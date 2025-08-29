d1787ba573777f235ba2918325c8f8c33247f0d1
David Swift <david.swift@magnopus.com>
2025-08-29 13:29:34 -0700

    [nt-0] test: check behaviour of pointers in a simple class
    Added some more detail to the basic tests of an empty generated class.
    Demonstrates that while the wrapped class prevents calling the
    destructor multiple times, the actual pointer remains valid.

--- UnitTesting/tests/csharp/Assets/Tests/ClassTests.cs
+++ UnitTesting/tests/csharp/Assets/Tests/ClassTests.cs
@@ -23,8 +23,44 @@ namespace Csp.Tests
         [Test]
         public void CreateAndDestroySimpleClass()
         {
+            // Calling the constructor creates an instance of the native class.
+            var simpleClass = new SimpleClass();
+            Assert.IsTrue(simpleClass.PointerIsValid);
+
+            // Verify the internal pointer is set.
+            // Note that we have to use reflection here to access the internal field.
+            var ptrField = typeof(SimpleClass).GetField("_ptr", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var ptrValue = ptrField.GetValue(simpleClass);
+            Assert.IsNotNull(ptrValue);
+
+            // Verify that the instance owns the native pointer.
+            // Note that we have to use reflection here to access the internal field.
+            var ownsPtrField = typeof(SimpleClass).GetField("_ownsPtr", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            var ownsPtrValue = ownsPtrField.GetValue(simpleClass);
+            Assert.IsTrue((bool)ownsPtrValue);
+
+            // Calling Dispose will call the destructor on the native class and
+            // mark this instance as disposed.
+            simpleClass.Dispose();
+
+            // Once Dispose is called, the internal pointer is no longer valid.
+            // However, Dispose does not null out the pointer so the class is
+            // left in an indeterminate state.
+            // There is also no way to query whether Dispose has been called.
+            Assert.IsTrue(simpleClass.PointerIsValid);
+
+            // Verify the internal pointer is still set.
+            var ptrValueDisposed = ptrField.GetValue(simpleClass);
+            Assert.IsNotNull(ptrValueDisposed);
+        }
+
+        [Test]
+        public void MultipleDisposes()
+        {
             var simpleClass = new SimpleClass();
 
+            // Call Dispose multiple times and ensure no errors occur.
+            simpleClass.Dispose();
             simpleClass.Dispose();
         }
     }
