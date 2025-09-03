713b81c7587d744ab88fa4abbc3dde0ad6da2050
MAG-mv <matthew.voisey@magnopus.com>
2025-09-03 15:16:12 +0100

    [OF-1744] test: Adding new unit tests for SceneDescription/Data
    These are unit tests used without the realtimeengine
    
    We will do full integration tests in a later commit

--- Tests/src/InternalTests/SceneDescriptionTests.cpp
+++ Tests/src/InternalTests/SceneDescriptionTests.cpp
@@ -20,10 +20,15 @@
 #include "CSP/Common/Interfaces/IAuthContext.h"
 #include "CSP/Common/Systems/Log/LogSystem.h"
 #include "CSP/Multiplayer/CSPSceneDescription.h"
+#include "CSP/Multiplayer/Components/StaticModelSpaceComponent.h"
 #include "CSP/Multiplayer/OnlineRealtimeEngine.h"
+#include "CSP/Systems/Assets/AssetSystem.h"
 #include "CSP/Systems/CSPSceneData.h"
+#include "CSP/Systems/SystemsManager.h"
+#include "CSP/Systems/Users/UserSystem.h"
 #include "Multiplayer/MCS/MCSSceneDescription.h"
 #include "Multiplayer/MCS/MCSTypes.h"
+#include "PublicAPITests/UserSystemTestHelpers.h"
 #include "Json/JsonSerializer.h"
 
 #include <filesystem>
@@ -31,6 +36,11 @@
 
 using namespace csp::multiplayer;
 using namespace csp::systems;
+
+namespace
+{
+bool RequestPredicate(const csp::systems::ResultBase& Result) { return Result.GetResultCode() != csp::systems::EResultCode::InProgress; }
+}
 
 CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, ObjectMessageSerializeTest)
 {
@@ -249,11 +259,26 @@ private:
     csp::common::LoginState State;
 };
 
+/*
+    CSPSceneDescription / CSPSceneData tests from a generated checkpoint file.
+
+    The checkpoint files for these tests were generated using the following steps:
+        - Create a space using a test in the OKO_TESTS test tenant
+        - Log into an admin account on swagger for the OKO_TESTS tenant
+        - Call spaces/{spaceId}/export using swagger to generate an export id
+        - Call spaces/{spaceId}/checkpoints to generate a checkpoint asset
+        - Create another test downloading the asset that is generated from the checkpoints call
+        - Copy the downloaded checkpoint json into a file
+
+    If we wanted to create a large amount of these, we should probably write a test to automate this process.
+*/
+
+// Tests SceneData/SceneDescription can be parsed from an empty checkpoint file.
 CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionDeserializeEmptyTest)
 {
     InitialiseFoundationWithUserAgentInfo(EndpointBaseURI());
 
-    auto FilePath = std::filesystem::absolute("assets/checkpoint-example-empty.json");
+    auto FilePath = std::filesystem::absolute("assets/checkpoint-empty.json");
 
     std::ifstream Stream { FilePath.u8string().c_str() };
 
@@ -277,6 +302,9 @@ CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionDeserializeE
 
     CSPSceneDescription SceneDescription { Json.c_str(), EntitySystem, LogSystem, ScriptRunner };
     CSPSceneData SceneData { Json.c_str() };
+
+    EXPECT_EQ(SceneData.Space.Id, "68addce4985d7612f76b9461");
+    EXPECT_EQ(SceneData.Space.Name, "checkpoint-empty");
 
     EXPECT_EQ(SceneDescription.Entities.Size(), 0);
     EXPECT_EQ(SceneData.AssetCollections.Size(), 0);
@@ -286,11 +314,13 @@ CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionDeserializeE
     csp::CSPFoundation::Shutdown();
 }
 
-CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionDeserializeTest)
+// Tests SceneData/SceneDescription can be parsed from an basic checkpoint file.
+// This file contains one of every item exposed by the scene description, except anchors.
+CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionDeserializeBasicTest)
 {
     InitialiseFoundationWithUserAgentInfo(EndpointBaseURI());
 
-    auto FilePath = std::filesystem::absolute("assets/checkpoint-example.json");
+    auto FilePath = std::filesystem::absolute("assets/checkpoint-basic.json");
 
     std::ifstream Stream { FilePath.u8string().c_str() };
 
@@ -307,6 +337,7 @@ CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionDeserializeT
     MockScriptRunner ScriptRunner;
     csp::common::LogSystem LogSystem;
     TestAuthContext AuthContext;
+
     csp::multiplayer::MultiplayerConnection Connection { LogSystem, *csp::multiplayer::MultiplayerConnection::MakeSignalRConnection(AuthContext) };
     csp::multiplayer::NetworkEventBus NetworkEventBus { &Connection, LogSystem };
     csp::multiplayer::OnlineRealtimeEngine EntitySystem(Connection, LogSystem, NetworkEventBus, ScriptRunner);
@@ -314,91 +345,80 @@ CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionDeserializeT
     CSPSceneDescription SceneDescription { Json.c_str(), EntitySystem, LogSystem, ScriptRunner };
     CSPSceneData SceneData { Json.c_str() };
 
-    // Test csp scene description values are correct
+    EXPECT_EQ(SceneData.Space.Id, "68af162f015bb6793cacf4a2");
+    EXPECT_EQ(SceneData.Space.Name, "checkpoint-basic");
 
-    // Space
-    EXPECT_EQ(SceneData.Space.Id, "66c65e8d9821e1cc2b51dc0d");
-    EXPECT_EQ(SceneData.Space.CreatedBy, "66a0033d6541645960bfffda");
-    EXPECT_EQ(SceneData.Space.CreatedAt, "2024-08-21T21:39:25.017+00:00");
-    EXPECT_EQ(SceneData.Space.OwnerId, "66a0033d6541645960bfffda");
-    EXPECT_EQ(SceneData.Space.Name, "Abu Dhabi Airport");
-    EXPECT_EQ(SceneData.Space.UserIds.Size(), 21);
-    EXPECT_EQ(SceneData.Space.UserIds[0], "66a0033d6541645960bfffda");
-    EXPECT_EQ(SceneData.Space.UserIds[20], "6823720f8f72b4d0fa153cfd");
-    EXPECT_EQ(SceneData.Space.BannedUserIds.Size(), 0);
-    EXPECT_EQ(SceneData.Space.ModeratorIds.Size(), 20);
-    EXPECT_EQ(SceneData.Space.ModeratorIds[0], "669ac6673d223b140719c19e");
-    EXPECT_EQ(SceneData.Space.ModeratorIds[19], "6823720f8f72b4d0fa153cfd");
+    // Ensure arrays are the size we expect before continuing.
+    if (SceneDescription.Entities.Size() != 1)
+    {
+        FAIL();
+    }
 
-    EXPECT_FALSE(csp::systems::HasFlag(SceneData.Space.Attributes, csp::systems::SpaceAttributes::IsDiscoverable));
-    EXPECT_TRUE(csp::systems::HasFlag(SceneData.Space.Attributes, csp::systems::SpaceAttributes::RequiresInvite));
+    if (SceneData.AssetCollections.Size() != 1)
+    {
+        FAIL();
+    }
 
-    // Entities
-    EXPECT_EQ(SceneDescription.Entities.Size(), 74);
+    if (SceneData.Assets.Size() != 1)
+    {
+        FAIL();
+    }
 
-    csp::multiplayer::SpaceEntity* SpaceEntity = SceneDescription.Entities[0];
-    EXPECT_EQ(SpaceEntity->GetId(), 1484);
-    EXPECT_EQ(SpaceEntity->GetEntityType(), csp::multiplayer::SpaceEntityType::Object);
-    EXPECT_EQ(SpaceEntity->IsTransferable, true);
-    EXPECT_EQ(SpaceEntity->IsPersistent, true);
-    // TODO: ownerid
-    EXPECT_FALSE(SpaceEntity->ParentId.HasValue());
+    if (SceneData.Sequences.Size() != 1)
+    {
+        FAIL();
+    }
 
-    // AssetCollection
-    EXPECT_EQ(SceneData.AssetCollections.Size(), 88);
+    // Check entity is parsed correctly.
+    csp::multiplayer::SpaceEntity* Entity = SceneDescription.Entities[0];
+    EXPECT_EQ(Entity->GetName(), "Entity");
 
-    csp::systems::AssetCollection AssetCollection = SceneData.AssetCollections[0];
-    EXPECT_EQ(AssetCollection.GetMetadataMutable().Size(), 1);
-    EXPECT_EQ(AssetCollection.GetMetadataMutable()["assetType"], csp::common::String { "staticmodel" });
-    EXPECT_EQ(AssetCollection.Id, "6807d74f486cf8794b73f107");
-    EXPECT_EQ(AssetCollection.Name, "Small_Kiosk_T.glb_536.4520788603719");
-    EXPECT_EQ(AssetCollection.Type, csp::systems::EAssetCollectionType::DEFAULT);
-    EXPECT_EQ(AssetCollection.Tags.Size(), 0);
-    EXPECT_EQ(AssetCollection.PointOfInterestId, "");
-    EXPECT_EQ(AssetCollection.ParentId, "");
-    EXPECT_EQ(AssetCollection.SpaceId, "66c65e8d9821e1cc2b51dc0d");
-    EXPECT_EQ(AssetCollection.CreatedBy, "669ac6673d223b140719c19e");
-    EXPECT_EQ(AssetCollection.CreatedAt, "2025-04-22T17:52:15.369+00:00");
-    EXPECT_EQ(AssetCollection.UpdatedBy, "669ac6673d223b140719c19e");
-    EXPECT_EQ(AssetCollection.UpdatedAt, "2025-04-22T17:52:15.369+00:00");
-    EXPECT_EQ(AssetCollection.IsUnique, false);
-    EXPECT_EQ(AssetCollection.Version, "");
+    if (Entity->GetComponents()->Size() != 1)
+    {
+        FAIL();
+    }
 
-    // Asset
-    EXPECT_EQ(SceneData.Assets.Size(), 488);
+    if (Entity->GetComponent(0)->GetComponentType() != csp::multiplayer::ComponentType::StaticModel)
+    {
+        FAIL();
+    }
 
-    csp::systems::Asset Asset = SceneData.Assets[0];
-    EXPECT_EQ(Asset.AssetCollectionId, "6807d74f486cf8794b73f107");
-    EXPECT_EQ(Asset.Id, "6807d750b618cc273309a258");
-    EXPECT_EQ(Asset.FileName, "");
-    EXPECT_EQ(Asset.Name, "Small_Kiosk_T_lod3.glb");
-    EXPECT_EQ(Asset.LanguageCode, "en-us");
-    EXPECT_EQ(Asset.Type, csp::systems::EAssetType::MODEL);
-    EXPECT_EQ(Asset.Platforms.Size(), 1);
-    EXPECT_EQ(Asset.Platforms[0], csp::systems::EAssetPlatform::DEFAULT);
-    EXPECT_EQ(Asset.Styles.Size(), 2);
-    EXPECT_EQ(Asset.Styles[0], "Default");
-    EXPECT_EQ(Asset.Styles[1], "lod:3");
-    EXPECT_EQ(Asset.ExternalUri, "");
-    EXPECT_EQ(Asset.Uri, "");
-    EXPECT_EQ(Asset.Checksum, "");
-    EXPECT_EQ(Asset.Version, 0);
-    EXPECT_EQ(Asset.MimeType, "");
-    EXPECT_EQ(Asset.ExternalMimeType, "");
-    EXPECT_EQ(Asset.ThirdPartyPackagedAssetIdentifier, "");
-    EXPECT_EQ(Asset.ThirdPartyPlatformType, csp::systems::EThirdPartyPlatform::NONE);
+    auto StaticModelComponent = static_cast<csp::multiplayer::StaticModelSpaceComponent*>(Entity->GetComponent(0));
+    EXPECT_EQ(StaticModelComponent->GetExternalResourceAssetCollectionId(), "TestAssetCollectionId");
 
-    // Sequences
-    EXPECT_EQ(SceneData.Sequences.Size(), 0);
+    // Test asset collection is parsed correctly.
+    const csp::systems::AssetCollection& Collection = SceneData.AssetCollections[0];
+    EXPECT_EQ(Collection.Name, "BasicCheckpointAssetCollection2");
+    EXPECT_EQ(Collection.SpaceId, "68af162f015bb6793cacf4a2");
+
+    // Test asset is parsed correctly
+    const csp::systems::Asset& Asset = SceneData.Assets[0];
+    EXPECT_EQ(Asset.Name, "BasicCheckpointAsset2");
+    EXPECT_EQ(Asset.AssetCollectionId, Collection.Id);
+
+    // Test sequence is parsed correctly
+    // We use * as this gets encoded, so we want to ensure the sequence is correctly decoded.
+    const csp::systems::Sequence& Sequence = SceneData.Sequences[0];
+    EXPECT_EQ(Sequence.Key, "*BasicCheckpointSequence2*");
+
+    if (Sequence.Items.Size() != 3)
+    {
+        FAIL();
+    }
+
+    EXPECT_EQ(Sequence.Items[0], "1");
+    EXPECT_EQ(Sequence.Items[1], "2");
+    EXPECT_EQ(Sequence.Items[2], "3");
 
     csp::CSPFoundation::Shutdown();
 }
 
-CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionMinimalDeserializeTest)
+// Tests that a material parsed from scene data is valid
+CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionDeserializeMaterialTest)
 {
     InitialiseFoundationWithUserAgentInfo(EndpointBaseURI());
 
-    auto FilePath = std::filesystem::absolute("assets/checkpoint-example-minimal.json");
+    auto FilePath = std::filesystem::absolute("assets/checkpoint-material.json");
 
     std::ifstream Stream { FilePath.u8string().c_str() };
 
@@ -412,87 +432,34 @@ CSP_INTERNAL_TEST(CSPEngine, SceneDescriptionTests, SceneDescriptionMinimalDeser
 
     std::string Json = SStream.str();
 
-    MockScriptRunner ScriptRunner;
-    csp::common::LogSystem LogSystem;
-    TestAuthContext AuthContext;
-    csp::multiplayer::MultiplayerConnection Connection { LogSystem, *csp::multiplayer::MultiplayerConnection::MakeSignalRConnection(AuthContext) };
-    csp::multiplayer::NetworkEventBus NetworkEventBus { &Connection, LogSystem };
-    csp::multiplayer::OnlineRealtimeEngine EntitySystem(Connection, LogSystem, NetworkEventBus, ScriptRunner);
-
-    CSPSceneDescription SceneDescription { Json.c_str(), EntitySystem, LogSystem, ScriptRunner };
     CSPSceneData SceneData { Json.c_str() };
+    EXPECT_EQ(SceneData.Space.Name, "checkpoint-material");
 
-    // Test csp scene description values are correct
+    auto& SystemsManager = csp::systems::SystemsManager::Get();
+    auto* AssetSystem = SystemsManager.GetAssetSystem();
 
-    // Space
-    EXPECT_EQ(SceneData.Space.Id, "66c65e8d9821e1cc2b51dc0d");
-    EXPECT_EQ(SceneData.Space.CreatedBy, "66a0033d6541645960bfffda");
-    EXPECT_EQ(SceneData.Space.CreatedAt, "2024-08-21T21:39:25.017+00:00");
-    EXPECT_EQ(SceneData.Space.OwnerId, "66a0033d6541645960bfffda");
-    EXPECT_EQ(SceneData.Space.Name, "Abu Dhabi Airport");
-    EXPECT_EQ(SceneData.Space.UserIds.Size(), 0);
-    EXPECT_EQ(SceneData.Space.BannedUserIds.Size(), 0);
-    EXPECT_EQ(SceneData.Space.ModeratorIds.Size(), 0);
+    if (SceneData.AssetCollections.Size() != 1)
+    {
+        FAIL();
+    }
 
-    EXPECT_FALSE(csp::systems::HasFlag(SceneData.Space.Attributes, csp::systems::SpaceAttributes::IsDiscoverable));
-    EXPECT_TRUE(csp::systems::HasFlag(SceneData.Space.Attributes, csp::systems::SpaceAttributes::RequiresInvite));
+    if (SceneData.Assets.Size() != 1)
+    {
+        FAIL();
+    }
 
-    // Entities
-    EXPECT_EQ(SceneDescription.Entities.Size(), 1);
+    const csp::systems::AssetCollection& Collection = SceneData.AssetCollections[0];
+    const csp::systems::Asset& Asset = SceneData.Assets[0];
 
-    csp::multiplayer::SpaceEntity* SpaceEntity = SceneDescription.Entities[0];
-    EXPECT_EQ(SpaceEntity->GetId(), 1484);
-    EXPECT_EQ(SpaceEntity->GetEntityType(), csp::multiplayer::SpaceEntityType::Object);
-    EXPECT_EQ(SpaceEntity->IsTransferable, true);
-    EXPECT_EQ(SpaceEntity->IsPersistent, true);
-    // TODO: ownerid
-    EXPECT_FALSE(SpaceEntity->ParentId.HasValue());
+    auto [MaterialResult] = AWAIT_PRE(AssetSystem, GetMaterialFromUri, RequestPredicate, Collection, Asset.Id, Asset.Uri);
+    EXPECT_EQ(MaterialResult.GetResultCode(), csp::systems::EResultCode::Success);
 
-    // AssetCollection
-    EXPECT_EQ(SceneData.AssetCollections.Size(), 1);
+    const csp::systems::Material* Material = MaterialResult.GetMaterial();
 
-    csp::systems::AssetCollection AssetCollection = SceneData.AssetCollections[0];
-    EXPECT_EQ(AssetCollection.GetMetadataMutable().Size(), 1);
-    EXPECT_EQ(AssetCollection.GetMetadataMutable()["assetType"], csp::common::String { "staticmodel" });
-    EXPECT_EQ(AssetCollection.Id, "6807d74f486cf8794b73f107");
-    EXPECT_EQ(AssetCollection.Name, "Small_Kiosk_T.glb_536.4520788603719");
-    EXPECT_EQ(AssetCollection.Type, csp::systems::EAssetCollectionType::DEFAULT);
-    EXPECT_EQ(AssetCollection.Tags.Size(), 0);
-    EXPECT_EQ(AssetCollection.PointOfInterestId, "");
-    EXPECT_EQ(AssetCollection.ParentId, "");
-    EXPECT_EQ(AssetCollection.SpaceId, "66c65e8d9821e1cc2b51dc0d");
-    EXPECT_EQ(AssetCollection.CreatedBy, "669ac6673d223b140719c19e");
-    EXPECT_EQ(AssetCollection.CreatedAt, "2025-04-22T17:52:15.369+00:00");
-    EXPECT_EQ(AssetCollection.UpdatedBy, "669ac6673d223b140719c19e");
-    EXPECT_EQ(AssetCollection.UpdatedAt, "2025-04-22T17:52:15.369+00:00");
-    EXPECT_EQ(AssetCollection.IsUnique, false);
-    EXPECT_EQ(AssetCollection.Version, "");
-
-    // Asset
-    EXPECT_EQ(SceneData.Assets.Size(), 1);
-
-    csp::systems::Asset Asset = SceneData.Assets[0];
-    EXPECT_EQ(Asset.AssetCollectionId, "6807d74f486cf8794b73f107");
-    EXPECT_EQ(Asset.Id, "6807d750b618cc273309a258");
-    EXPECT_EQ(Asset.FileName, "");
-    EXPECT_EQ(Asset.Name, "Small_Kiosk_T_lod3.glb");
-    EXPECT_EQ(Asset.LanguageCode, "en-us");
-    EXPECT_EQ(Asset.Type, csp::systems::EAssetType::MODEL);
-    EXPECT_EQ(Asset.Platforms.Size(), 1);
-    EXPECT_EQ(Asset.Platforms[0], csp::systems::EAssetPlatform::DEFAULT);
-    EXPECT_EQ(Asset.Styles.Size(), 1);
-    EXPECT_EQ(Asset.Styles[0], "Default");
-    EXPECT_EQ(Asset.ExternalUri, "");
-    EXPECT_EQ(Asset.Uri, "");
-    EXPECT_EQ(Asset.Checksum, "");
-    EXPECT_EQ(Asset.Version, 0);
-    EXPECT_EQ(Asset.MimeType, "");
-    EXPECT_EQ(Asset.ExternalMimeType, "");
-    EXPECT_EQ(Asset.ThirdPartyPackagedAssetIdentifier, "");
-    EXPECT_EQ(Asset.ThirdPartyPlatformType, csp::systems::EThirdPartyPlatform::NONE);
-
-    // Sequences
-    EXPECT_EQ(SceneData.Sequences.Size(), 0);
+    EXPECT_EQ(Material->GetName(), "Material");
+    EXPECT_EQ(Material->GetShaderType(), csp::systems::EShaderType::Standard);
+    EXPECT_EQ(Material->GetMaterialCollectionId(), Collection.Id);
+    EXPECT_EQ(Material->GetMaterialId(), Asset.Id);
 
     csp::CSPFoundation::Shutdown();
 }
