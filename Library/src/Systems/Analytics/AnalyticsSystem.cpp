f12dfd19962c744a20850f14d2b1a63a71263ff4
MAG-AdamThorn <adam.thorn@magnopus.com>
2025-09-12 17:40:43 +0100

    [OF-1739] test: Add batched analytics events

--- Library/src/Systems/Analytics/AnalyticsSystem.cpp
+++ Library/src/Systems/Analytics/AnalyticsSystem.cpp
@@ -18,6 +18,8 @@
 
 #include "CSP/CSPFoundation.h"
 #include "CallHelpers.h"
+#include "Events/EventListener.h"
+#include "Events/EventSystem.h"
 #include "Services/UserService/Api.h"
 #include "Services/UserService/Dto.h"
 #include "Systems/ResultHelpers.h"
@@ -29,36 +31,12 @@ using namespace csp::common;
 
 namespace chs = csp::services::generated::userservice;
 
-namespace csp::systems
+namespace
 {
 
-AnalyticsSystem::AnalyticsSystem()
-    : SystemBase(nullptr, nullptr, nullptr)
-    , AnalyticsApi(nullptr)
-    , UserAgentInfo(nullptr)
+std::shared_ptr<chs::AnalyticsRecord> CreateAnalyticsRecord(const csp::ClientUserAgent* UserAgentInfo, const String& ProductContextSection,
+    const String& Category, const String& InteractionType, const Optional<String>& SubCategory, const Optional<Map<String, String>>& Metadata)
 {
-}
-
-AnalyticsSystem::AnalyticsSystem(csp::web::WebClient* InWebClient, const csp::ClientUserAgent* AgentInfo, csp::common::LogSystem& LogSystem)
-    : SystemBase(InWebClient, nullptr, &LogSystem)
-{
-    AnalyticsApi = std::unique_ptr<csp::services::ApiBase>(new chs::AnalyticsApi(InWebClient));
-    UserAgentInfo = AgentInfo;
-}
-
-AnalyticsSystem::~AnalyticsSystem() { }
-
-void AnalyticsSystem::SendAnalyticsEvent(const String& ProductContextSection, const String& Category, const String& InteractionType,
-    const Optional<String>& SubCategory, const Optional<Map<String, String>>& Metadata, NullResultCallback Callback)
-{
-    if (ProductContextSection.IsEmpty() || Category.IsEmpty() || InteractionType.IsEmpty())
-    {
-        CSP_LOG_MSG(common::LogLevel::Error, "Missing the required fields for the Analytics Event.");
-        INVOKE_IF_NOT_NULL(Callback, MakeInvalid<NullResult>());
-
-        return;
-    }
-
     // Construct an AnalyticsRecord object
     auto Record = std::make_shared<chs::AnalyticsRecord>();
     Record->SetProductIdentifier("csp");
@@ -89,6 +67,103 @@ void AnalyticsSystem::SendAnalyticsEvent(const String& ProductContextSection, co
 
     Record->SetTimestamp(DateTime::TimeNow().GetUtcString());
 
+    return Record;
+}
+
+}
+
+namespace csp::systems
+{
+
+class AnalyticsBatchEventHandler : public csp::events::EventListener
+{
+public:
+    AnalyticsBatchEventHandler(AnalyticsSystem* AnalyticsSystem);
+
+    void OnEvent(const csp::events::Event& InEvent) override;
+
+private:
+    AnalyticsSystem* _AnalyticsSystem;
+};
+
+AnalyticsBatchEventHandler::AnalyticsBatchEventHandler(AnalyticsSystem* AnalyticsSystem)
+    : _AnalyticsSystem(AnalyticsSystem)
+{
+}
+
+void AnalyticsBatchEventHandler::OnEvent(const csp::events::Event& InEvent)
+{
+    if (InEvent.GetId() == csp::events::FOUNDATION_TICK_EVENT_ID && _AnalyticsSystem != nullptr)
+    {
+        _AnalyticsSystem->ProcessBatchedAnalyticsRecords();
+    }
+}
+
+AnalyticsSystem::AnalyticsSystem()
+    : SystemBase(nullptr, nullptr, nullptr)
+    , EventHandler(nullptr)
+    , AnalyticsBatchLock(new std::recursive_mutex)
+    , AnalyticsApi(nullptr)
+    , AnalyticsRecordBatch(nullptr)
+    , UserAgentInfo(nullptr)
+    , AnalyticsBatchRate(std::chrono::seconds(60))
+    , TimeOfLastBatch(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()))
+    , MaxBatchSize(25)
+    , IsFlushingAnalyticsEvents(false)
+{
+}
+
+AnalyticsSystem::AnalyticsSystem(csp::web::WebClient* InWebClient, const csp::ClientUserAgent* AgentInfo, csp::common::LogSystem& LogSystem)
+    : SystemBase(InWebClient, nullptr, &LogSystem)
+    , EventHandler(new AnalyticsBatchEventHandler(this))
+    , AnalyticsBatchLock(new std::recursive_mutex)
+    , AnalyticsRecordBatch(new(std::deque<std::shared_ptr<chs::AnalyticsRecord>>))
+    , UserAgentInfo(AgentInfo)
+    , AnalyticsBatchRate(std::chrono::seconds(60))
+    , TimeOfLastBatch(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()))
+    , MaxBatchSize(25)
+    , IsFlushingAnalyticsEvents(false)
+{
+    AnalyticsApi = std::unique_ptr<csp::services::ApiBase>(new chs::AnalyticsApi(InWebClient));
+
+    csp::events::EventSystem::Get().RegisterListener(csp::events::FOUNDATION_TICK_EVENT_ID, EventHandler);
+}
+
+AnalyticsSystem::~AnalyticsSystem()
+{
+    csp::events::EventSystem::Get().UnRegisterListener(csp::events::FOUNDATION_TICK_EVENT_ID, EventHandler);
+
+    delete (EventHandler);
+    delete (AnalyticsBatchLock);
+    delete (AnalyticsRecordBatch);
+}
+
+std::chrono::milliseconds AnalyticsSystem::GetTimeOfLastBatch() const { return TimeOfLastBatch; }
+
+void AnalyticsSystem::SetTimeOfLastBatch(std::chrono::milliseconds NewTimeOfLastBatch) { TimeOfLastBatch = NewTimeOfLastBatch; }
+
+// This is a utility function to allow us to test the batching functionality in a reasonable time frame.
+void AnalyticsSystem::SetBatchRateAndSize(std::chrono::milliseconds NewBatchRate, size_t NewBatchSize)
+{
+    std::scoped_lock AnalyticsBatchLocker(*AnalyticsBatchLock);
+
+    AnalyticsBatchRate = NewBatchRate;
+    MaxBatchSize = NewBatchSize;
+}
+
+void AnalyticsSystem::SendAnalyticsEvent(const String& ProductContextSection, const String& Category, const String& InteractionType,
+    const Optional<String>& SubCategory, const Optional<Map<String, String>>& Metadata, NullResultCallback Callback)
+{
+    if (ProductContextSection.IsEmpty() || Category.IsEmpty() || InteractionType.IsEmpty())
+    {
+        CSP_LOG_MSG(common::LogLevel::Error, "Missing the required fields for the Analytics Event.");
+        INVOKE_IF_NOT_NULL(Callback, MakeInvalid<NullResult>());
+
+        return;
+    }
+
+    auto Record = CreateAnalyticsRecord(UserAgentInfo, ProductContextSection, Category, InteractionType, SubCategory, Metadata);
+
     std::vector<std::shared_ptr<chs::AnalyticsRecord>> Records;
     Records.push_back(Record);
 
@@ -118,6 +193,94 @@ void AnalyticsSystem::SendAnalyticsEvent(const String& ProductContextSection, co
 
     csp::services::ResponseHandlerPtr ResponseHandler
         = AnalyticsApi->CreateHandler<NullResultCallback, NullResult, void, chs::AnalyticsRecord>(SendAnalyticsCallback, nullptr);
+
+    static_cast<chs::AnalyticsApi*>(AnalyticsApi.get())->analyticsBulkPost(Records, ResponseHandler);
+}
+
+void AnalyticsSystem::BatchAnalyticsEvent(const String& ProductContextSection, const String& Category, const String& InteractionType,
+    const Optional<String>& SubCategory, const Optional<Map<String, String>>& Metadata)
+{
+    if (ProductContextSection.IsEmpty() || Category.IsEmpty() || InteractionType.IsEmpty())
+    {
+        CSP_LOG_MSG(common::LogLevel::Error, "Missing the required fields for the Analytics Event.");
+
+        return;
+    }
+
+    auto Record = CreateAnalyticsRecord(UserAgentInfo, ProductContextSection, Category, InteractionType, SubCategory, Metadata);
+
+    std::scoped_lock AnalyticsBatchLocker(*AnalyticsBatchLock);
+    AnalyticsRecordBatch->emplace_back(Record);
+}
+
+void AnalyticsSystem::ProcessBatchedAnalyticsRecords()
+{
+    std::scoped_lock AnalyticsBatchLocker(*AnalyticsBatchLock);
+
+    if (AnalyticsRecordBatch->empty())
+    {
+        return;
+    }
+
+    const std::chrono::milliseconds CurrentTime
+        = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+    if (CurrentTime - GetTimeOfLastBatch() >= AnalyticsBatchRate)
+    {
+        FlushAnalyticsEvents();
+        return;
+    }
+
+    if (AnalyticsRecordBatch->size() >= MaxBatchSize)
+    {
+        FlushAnalyticsEvents();
+        return;
+    }
+}
+
+void AnalyticsSystem::FlushAnalyticsEvents()
+{
+    std::scoped_lock AnalyticsBatchLocker(*AnalyticsBatchLock);
+
+    if (AnalyticsRecordBatch->empty())
+    {
+        return;
+    }
+
+    const std::chrono::milliseconds CurrentTime
+        = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+    SetTimeOfLastBatch(CurrentTime);
+
+    std::vector<std::shared_ptr<chs::AnalyticsRecord>> Records;
+    Records.reserve(AnalyticsRecordBatch->size());
+
+    while (AnalyticsRecordBatch->empty() == false)
+    {
+        auto Record = AnalyticsRecordBatch->front();
+
+        Records.push_back(Record);
+
+        AnalyticsRecordBatch->pop_front();
+    }
+
+    NullResultCallback SendBatchAnalyticsCallback = [LogSystem = this->LogSystem, BatchSize = Records.size()](const NullResult& Result)
+    {
+        if (Result.GetResultCode() == csp::systems::EResultCode::Success)
+        {
+            LogSystem->LogMsg(common::LogLevel::Verbose, "Batched Analytics Events sent.");
+        }
+        else if (Result.GetResultCode() == csp::systems::EResultCode::Failed)
+        {
+            LogSystem->LogMsg(common::LogLevel::Error,
+                fmt::format("Failed to send Batch Analytics Event. ResCode: {}, HttpResCode: {}", static_cast<int>(Result.GetResultCode()),
+                    Result.GetHttpResultCode())
+                    .c_str());
+        }
+    };
+
+    csp::services::ResponseHandlerPtr ResponseHandler
+        = AnalyticsApi->CreateHandler<NullResultCallback, NullResult, void, chs::AnalyticsRecord>(SendBatchAnalyticsCallback, nullptr);
 
     static_cast<chs::AnalyticsApi*>(AnalyticsApi.get())->analyticsBulkPost(Records, ResponseHandler);
 }
--- Tests/src/PublicAPITests/AnalyticsSystemTests.cpp
+++ Tests/src/PublicAPITests/AnalyticsSystemTests.cpp
@@ -36,6 +36,9 @@ bool RequestPredicate(const csp::systems::ResultBase& Result) { return Result.Ge
 
 } // namespace
 
+/*
+ * Test that we can successfully send an analytics event
+ */
 CSP_PUBLIC_TEST(CSPEngine, AnalyticsSystemTests, SendAnalyticsEventTest)
 {
     SetRandSeed();
@@ -66,6 +69,9 @@ CSP_PUBLIC_TEST(CSPEngine, AnalyticsSystemTests, SendAnalyticsEventTest)
     LogOut(UserSystem);
 }
 
+/*
+ * Test that we get an error when sending an analytics event with a required field missing
+ */
 CSP_PUBLIC_TEST(CSPEngine, AnalyticsSystemTests, SendAnalyticsEventMissingFieldsTest)
 {
     SetRandSeed();
@@ -75,15 +81,18 @@ CSP_PUBLIC_TEST(CSPEngine, AnalyticsSystemTests, SendAnalyticsEventMissingFields
     auto& SystemsManager = csp::systems::SystemsManager::Get();
     auto* UserSystem = SystemsManager.GetUserSystem();
     auto* AnalyticsSystem = SystemsManager.GetAnalyticsSystem();
-    auto& LogSystem = *SystemsManager.GetLogSystem();
-
-    // We are only concerned with error logs in this test
-    LogSystem.SetSystemLevel(csp::common::LogLevel::Error);
 
     String UserId;
 
     // Log in
     LogInAsNewTestUser(UserSystem, UserId);
+
+    // Ensure the MockLogger will ignore all logs except the one we care about
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(::testing::_)).Times(::testing::AnyNumber());
+
+    // Ensure the required fields error message is logged when we try to send an analytics event with a required field missing
+    const csp::common::String AnalyticsErrorMsg = "Missing the required fields for the Analytics Event.";
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(AnalyticsErrorMsg)).Times(1);
 
     // Analytics Data
     // Passing an empty string for a required field
@@ -93,15 +102,284 @@ CSP_PUBLIC_TEST(CSPEngine, AnalyticsSystemTests, SendAnalyticsEventMissingFields
     const auto TestSubCategory = String("Event_SubCategory");
     const Map<String, String> TestMetadata = { { "Key1", "Value1" }, { "Key2", "Value2" } };
 
-    // Ensure the required fields error message is logged when we try to send an analytics event with a required field missing
-    const csp::common::String LockErrorMsg = "Missing the required fields for the Analytics Event.";
-    EXPECT_CALL(MockLogger.MockLogCallback, Call(LockErrorMsg)).Times(1);
-
     auto [Result] = AWAIT_PRE(AnalyticsSystem, SendAnalyticsEvent, RequestPredicate, TestProductContextSection, TestCategory, TestInteractionType,
         TestSubCategory, TestMetadata);
 
     EXPECT_EQ(Result.GetResultCode(), csp::systems::EResultCode::Failed);
     EXPECT_EQ(Result.GetHttpResultCode(), 0);
+
+    // Log out
+    LogOut(UserSystem);
+}
+
+/*
+ * Test that we can successfully send a batch of analytics events based on batch rate
+ * The batch size has been set high enough that it won't trigger the batch to be sent
+ */
+CSP_PUBLIC_TEST(CSPEngine, AnalyticsSystemTests, BatchSendAnalyticsEventBatchRateTest)
+{
+    SetRandSeed();
+
+    RAIIMockLogger MockLogger {};
+
+    auto& SystemsManager = csp::systems::SystemsManager::Get();
+    auto* UserSystem = SystemsManager.GetUserSystem();
+    auto* AnalyticsSystem = SystemsManager.GetAnalyticsSystem();
+
+    const std::chrono::milliseconds CurrentTime
+        = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+    // Reset time of last batch otherwise the analytics event batch will immediately be sent
+    AnalyticsSystem->SetTimeOfLastBatch(CurrentTime);
+    // The default batch rate and size are too large for testing so set them to something more reasonable
+    AnalyticsSystem->SetBatchRateAndSize(std::chrono::seconds(3), 5);
+
+    String UserId;
+
+    // Log in
+    LogInAsNewTestUser(UserSystem, UserId);
+
+    // Ensure the MockLogger will ignore all logs except the one we care about
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(::testing::_)).Times(::testing::AnyNumber());
+
+    // We can confirm that the batch has been successfully sent by checking for this success log message
+    const csp::common::String BatchAnalyticsSuccessMsg = "Batched Analytics Events sent.";
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(BatchAnalyticsSuccessMsg)).Times(1);
+
+    // Analytics Data
+    const auto TestProductContextSection = String("Event_ProductContextSection");
+    const auto TestCategory = String("Event_Category");
+    const auto TestInteractionType = String("Event_InteractionType");
+    const auto TestSubCategory = String("Event_SubCategory");
+    const Map<String, String> TestMetadata = { { "Key1", "Value1" }, { "Key2", "Value2" } };
+
+    // Send two analytics events to be queued for sending later as a batch
+    AnalyticsSystem->BatchAnalyticsEvent(TestProductContextSection, TestCategory, TestInteractionType, TestSubCategory, TestMetadata);
+    AnalyticsSystem->BatchAnalyticsEvent(TestProductContextSection, TestCategory, TestInteractionType, TestSubCategory, TestMetadata);
+
+    // The batch rate has been set to 3 seconds so we need to tick foundation for at least that long to allow the batch to be sent
+    auto Start = std::chrono::steady_clock::now();
+    auto Current = std::chrono::steady_clock::now();
+    long long TestTime = 0;
+
+    while (TestTime < 4)
+    {
+        std::this_thread::sleep_for(1s);
+
+        Current = std::chrono::steady_clock::now();
+        TestTime = std::chrono::duration_cast<std::chrono::seconds>(Current - Start).count();
+
+        csp::CSPFoundation::Tick();
+    }
+
+    // Log out
+    LogOut(UserSystem);
+}
+
+/*
+ * Test that we can successfully send a batch of analytics events based on batch size
+ * The batch rate has been set high enough that it won't trigger the batch to be sent
+ */
+CSP_PUBLIC_TEST(CSPEngine, AnalyticsSystemTests, BatchSendAnalyticsEventBatchSizeTest)
+{
+    SetRandSeed();
+
+    RAIIMockLogger MockLogger {};
+
+    auto& SystemsManager = csp::systems::SystemsManager::Get();
+    auto* UserSystem = SystemsManager.GetUserSystem();
+    auto* AnalyticsSystem = SystemsManager.GetAnalyticsSystem();
+
+    const std::chrono::milliseconds CurrentTime
+        = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+    // Reset time of last batch otherwise the analytics event batch will immediately be sent
+    AnalyticsSystem->SetTimeOfLastBatch(CurrentTime);
+    // The default batch rate and size are too large for testing so set them to something more reasonable
+    AnalyticsSystem->SetBatchRateAndSize(std::chrono::seconds(20), 3);
+
+    String UserId;
+
+    // Log in
+    LogInAsNewTestUser(UserSystem, UserId);
+
+    // Ensure the MockLogger will ignore all logs except the one we care about
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(::testing::_)).Times(::testing::AnyNumber());
+
+    auto SendBatchStart = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point LogTime;
+    bool BatchSent = false;
+    // We can confirm that the batch has been successfully sent by checking for this success log message
+    // We also check that the batch has been sent as a result of batch size rather than batch rate by checking
+    // the elapsed test time is less than the batch rate of 20 seconds
+    const csp::common::String BatchAnalyticsSuccessMsg = "Batched Analytics Events sent.";
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(BatchAnalyticsSuccessMsg))
+        .Times(1)
+        .WillOnce(::testing::Invoke(
+            [&](const csp::common::String&)
+            {
+                LogTime = std::chrono::steady_clock::now();
+                long long ElapsedTestTime = std::chrono::duration_cast<std::chrono::seconds>(LogTime - SendBatchStart).count();
+                EXPECT_TRUE(ElapsedTestTime < 20);
+                BatchSent = true;
+            }));
+
+    // Analytics Data
+    const auto TestProductContextSection = String("Event_ProductContextSection");
+    const auto TestCategory = String("Event_Category");
+    const auto TestInteractionType = String("Event_InteractionType");
+    const auto TestSubCategory = String("Event_SubCategory");
+    const Map<String, String> TestMetadata = { { "Key1", "Value1" }, { "Key2", "Value2" } };
+
+    // Send three analytics events to be queued for sending later as a batch
+    AnalyticsSystem->BatchAnalyticsEvent(TestProductContextSection, TestCategory, TestInteractionType, TestSubCategory, TestMetadata);
+    AnalyticsSystem->BatchAnalyticsEvent(TestProductContextSection, TestCategory, TestInteractionType, TestSubCategory, TestMetadata);
+    AnalyticsSystem->BatchAnalyticsEvent(TestProductContextSection, TestCategory, TestInteractionType, TestSubCategory, TestMetadata);
+
+    // The batch rate has been set to 20 seconds so we need to tick foundation for at least that long
+    // We check that the batch is sent as a result of batch size rather than batch rate in the EXPECT_CALL above
+    auto Start = std::chrono::steady_clock::now();
+    auto Current = std::chrono::steady_clock::now();
+    long long TestTime = 0;
+
+    // Once the batch has been set as a result of the BatchSize having been reached, BatchSent will be set to true
+    // in the EXPECT_CALL above and the while loop will exit before the 20 second batch rate is reached. This is
+    // also validated in the EXPECT_CALL above.
+    while (!BatchSent && TestTime < 21)
+    {
+        std::this_thread::sleep_for(1s);
+
+        Current = std::chrono::steady_clock::now();
+        TestTime = std::chrono::duration_cast<std::chrono::seconds>(Current - Start).count();
+
+        csp::CSPFoundation::Tick();
+    }
+
+    // Log out
+    LogOut(UserSystem);
+}
+
+/*
+ * Test that we get an error when attempting to batch send an analytics event with a required field missing
+ */
+CSP_PUBLIC_TEST(CSPEngine, AnalyticsSystemTests, BatchSendAnalyticsEventMissingFieldsTest)
+{
+    SetRandSeed();
+
+    RAIIMockLogger MockLogger {};
+
+    auto& SystemsManager = csp::systems::SystemsManager::Get();
+    auto* UserSystem = SystemsManager.GetUserSystem();
+    auto* AnalyticsSystem = SystemsManager.GetAnalyticsSystem();
+
+    String UserId;
+
+    // Log in
+    LogInAsNewTestUser(UserSystem, UserId);
+
+    // Ensure the MockLogger will ignore all logs except the one we care about
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(::testing::_)).Times(::testing::AnyNumber());
+
+    // Ensure the required fields error message is logged when we try to batch send an analytics event with a required field missing
+    const csp::common::String AnalyticsErrorMsg = "Missing the required fields for the Analytics Event.";
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(AnalyticsErrorMsg)).Times(1);
+
+    // Analytics Data
+    // Passing an empty string for a required field
+    const auto TestProductContextSection = String("");
+    const auto TestCategory = String("Event_Category");
+    const auto TestInteractionType = String("Event_InteractionType");
+    const auto TestSubCategory = String("Event_SubCategory");
+    const Map<String, String> TestMetadata = { { "Key1", "Value1" }, { "Key2", "Value2" } };
+
+    AnalyticsSystem->BatchAnalyticsEvent(TestProductContextSection, TestCategory, TestInteractionType, TestSubCategory, TestMetadata);
+
+    // Log out
+    LogOut(UserSystem);
+}
+
+/*
+ * Test that we can successfully flush a batch of analytics events.
+ * The batch rate and size has been set high enough that they won't trigger the batch to be sent.
+ */
+CSP_PUBLIC_TEST(CSPEngine, AnalyticsSystemTests, FlushAnalyticsEventsBatchTest)
+{
+    SetRandSeed();
+
+    RAIIMockLogger MockLogger {};
+
+    auto& SystemsManager = csp::systems::SystemsManager::Get();
+    auto* UserSystem = SystemsManager.GetUserSystem();
+    auto* AnalyticsSystem = SystemsManager.GetAnalyticsSystem();
+
+    const std::chrono::milliseconds CurrentTime
+        = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+    // Reset time of last batch otherwise the analytics event batch will immediately be sent
+    AnalyticsSystem->SetTimeOfLastBatch(CurrentTime);
+    // The default batch rate and size are too large for testing so set them to something more reasonable
+    AnalyticsSystem->SetBatchRateAndSize(std::chrono::seconds(20), 5);
+
+    String UserId;
+
+    // Log in
+    LogInAsNewTestUser(UserSystem, UserId);
+
+    // Ensure the MockLogger will ignore all logs except the one we care about
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(::testing::_)).Times(::testing::AnyNumber());
+
+    auto SendBatchStart = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point LogTime;
+    bool BatchFlushed = false;
+    // We can confirm that the batch has been successfully flushed by checking for this success log message
+    // We also check that the batch has been sent as a result of flush rather than batch rate by checking that
+    // the elapsed test time is less than the batch rate of 20 seconds
+    const csp::common::String BatchAnalyticsSuccessMsg = "Batched Analytics Events sent.";
+    EXPECT_CALL(MockLogger.MockLogCallback, Call(BatchAnalyticsSuccessMsg))
+        .Times(1)
+        .WillOnce(::testing::Invoke(
+            [&](const csp::common::String&)
+            {
+                LogTime = std::chrono::steady_clock::now();
+                long long ElapsedTestTime = std::chrono::duration_cast<std::chrono::seconds>(LogTime - SendBatchStart).count();
+                EXPECT_TRUE(ElapsedTestTime < 20);
+                BatchFlushed = true;
+            }));
+
+    // Analytics Data
+    const auto TestProductContextSection = String("Event_ProductContextSection");
+    const auto TestCategory = String("Event_Category");
+    const auto TestInteractionType = String("Event_InteractionType");
+    const auto TestSubCategory = String("Event_SubCategory");
+    const Map<String, String> TestMetadata = { { "Key1", "Value1" }, { "Key2", "Value2" } };
+
+    // Send two analytics events to be queued for sending later as a batch
+    AnalyticsSystem->BatchAnalyticsEvent(TestProductContextSection, TestCategory, TestInteractionType, TestSubCategory, TestMetadata);
+    AnalyticsSystem->BatchAnalyticsEvent(TestProductContextSection, TestCategory, TestInteractionType, TestSubCategory, TestMetadata);
+
+    // The batch rate has been set to 20 seconds so we need to tick foundation for at least that long to allow the batch to be sent
+    auto Start = std::chrono::steady_clock::now();
+    auto Current = std::chrono::steady_clock::now();
+    long long TestTime = 0;
+
+    // We will make the call to flush the batch after 3 seconds. If successful BatchFlushed will be set to true
+    // in the EXPECT_CALL above and the while loop will exit before the 20 second batch rate is reached. This is
+    // also validated in the EXPECT_CALL above.
+    while (!BatchFlushed && TestTime < 21)
+    {
+        std::this_thread::sleep_for(1s);
+
+        Current = std::chrono::steady_clock::now();
+        TestTime = std::chrono::duration_cast<std::chrono::seconds>(Current - Start).count();
+
+        if (TestTime == 3)
+        {
+            // Flush the batch of events after 3 seconds
+            AnalyticsSystem->FlushAnalyticsEvents();
+        }
+
+        csp::CSPFoundation::Tick();
+    }
 
     // Log out
     LogOut(UserSystem);
