eb551d2f956df202e625977decffb836ab590746
MAG-ChristopherAtkinson <Christopher.Atkinson@magnopus.global>
2025-10-08 09:22:59 +0100

    [OF-1772] test: add ai chatbot component script interface test

--- Tests/src/PublicAPITests/ComponentTests/AIChatbotComponentTests.cpp
+++ Tests/src/PublicAPITests/ComponentTests/AIChatbotComponentTests.cpp
@@ -117,3 +117,76 @@ CSP_PUBLIC_TEST(CSPEngine, AIChatbotTests, AIChatbotSpaceComponentTest)
     // Log out
     LogOut(UserSystem);
 }
+
+CSP_PUBLIC_TEST(CSPEngine, AIChatbotTests, AIChatbotSpaceComponentScriptTest)
+{
+    SetRandSeed();
+
+    auto& SystemsManager = csp::systems::SystemsManager::Get();
+    auto* UserSystem = SystemsManager.GetUserSystem();
+    auto* SpaceSystem = SystemsManager.GetSpaceSystem();
+
+    // Log in
+    csp::common::String UserId;
+    LogInAsNewTestUser(UserSystem, UserId);
+
+    // Create space
+    csp::systems::Space Space;
+    CreateDefaultTestSpace(SpaceSystem, Space);
+
+    std::unique_ptr<csp::multiplayer::OnlineRealtimeEngine> RealtimeEngine { SystemsManager.MakeOnlineRealtimeEngine() };
+    RealtimeEngine->SetEntityFetchCompleteCallback([](uint32_t) {});
+
+    auto [EnterResult] = AWAIT_PRE(SpaceSystem, EnterSpace, RequestPredicate, Space.Id, RealtimeEngine.get());
+
+    EXPECT_EQ(EnterResult.GetResultCode(), csp::systems::EResultCode::Success);
+
+    RealtimeEngine->SetRemoteEntityCreatedCallback([](csp::multiplayer::SpaceEntity* /*Entity*/) {});
+
+    // Create parent Space Entity
+    csp::common::String ObjectName = "Object 1";
+    SpaceTransform ObjectTransform = { csp::common::Vector3::Zero(), csp::common::Vector4::Zero(), csp::common::Vector3::One() };
+    auto [CreatedObject] = AWAIT(RealtimeEngine.get(), CreateEntity, ObjectName, ObjectTransform, csp::common::Optional<uint64_t> {});
+
+    // Create ai chatbot component
+    auto* AIChatbotComponent = (AIChatbotSpaceComponent*)CreatedObject->AddComponent(ComponentType::AIChatbot);
+
+    CreatedObject->QueueUpdate();
+    RealtimeEngine->ProcessPendingEntityOperations();
+
+    // Setup script
+    const std::string ScreenSharingScriptText = R"xx(
+		var component = ThisEntity.getAIChatbotComponents()[0];
+
+		component.attenuationRadius = 22.0;
+		component.position = [1, 1, 1];
+		component.rotation = [1, 1, 1, 1];
+        component.scale = [0, 0, 0];
+        component.contextAssetId = "TEST_CONTEXT_ASSET_ID";
+        component.guardrailAssetId = "TEST_GUARDRAIL_ASSET_ID";
+        component.visualState = 0;
+    )xx";
+
+    CreatedObject->GetScript().SetScriptSource(ScreenSharingScriptText.c_str());
+    CreatedObject->GetScript().Invoke();
+
+    RealtimeEngine->ProcessPendingEntityOperations();
+
+    // Ensure values are set correctly
+    EXPECT_EQ(AIChatbotComponent->GetPosition(), csp::common::Vector3::One());
+    EXPECT_EQ(AIChatbotComponent->GetRotation(), csp::common::Vector4::One());
+    EXPECT_EQ(AIChatbotComponent->GetScale(), csp::common::Vector3::Zero());
+
+    EXPECT_EQ(AIChatbotComponent->GetContextAssetId(), "TEST_CONTEXT_ASSET_ID");
+    EXPECT_EQ(AIChatbotComponent->GetGuardrailAssetId(), "TEST_GUARDRAIL_ASSET_ID");
+
+    EXPECT_EQ(AIChatbotComponent->GetVisualState(), AIChatbotVisualState::None);
+
+    auto [ExitSpaceResult] = AWAIT_PRE(SpaceSystem, ExitSpace, RequestPredicate);
+
+    // Delete space
+    DeleteSpace(SpaceSystem, Space.Id);
+
+    // Log out
+    LogOut(UserSystem);
+}
