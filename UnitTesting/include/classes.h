53606c551422ba04d4647beab4fe12289b3e2cf9
David Swift <david.swift@magnopus.com>
2025-10-13 15:46:51 -0700

    [nt-0] fix: explicitly instantiate template types
    * Use template types in a dummy function to get the wrapper gen to actually generate functions
    * Left lots of comments for functions that don't work correctly

--- UnitTesting/include/classes.h
+++ UnitTesting/include/classes.h
@@ -16,6 +16,7 @@
 #pragma once
 
 #include "CSP/CSPCommon.h"
+#include "CSP/Common/String.h"
 
 namespace csp::Tests
 {
@@ -49,15 +50,38 @@ public:
     TemplateClass();
     ~TemplateClass();
 
+    void VoidFunction();
+
     void SetValue(const T& value);
 
-    // Plain return values are not supported by the code generator
-    // T GetValue() const;
+    // Return type must be const for correct wrapper generation
 
-    void GetValue(T& outValue) const;
+    // Doesn't work
+    // const T GetValue() const;
+    // void GetValue(T& value) const;
 
 private:
     T m_Value;
 };
+
+class CSP_API UsesTemplateClass
+{
+public:
+    // Dummy functions to force generation of C wrapper functions for specific template types
+    // Note that the type must be fully qualified here!
+
+    // Doesn't work
+    // void DummyFunctionInt(const csp::Tests::TemplateClass<int>& obj);
+
+    const csp::Tests::TemplateClass<int>& DummyFunctionInt() const;
+    const csp::Tests::TemplateClass<csp::common::String>& DummyFunctionString() const;
+
+    // void DummyFunctionSimpleClass(const csp::Tests::TemplateClass<SimpleClass*>& obj);
+};
+
+// Doing this does NOT generate C wrapper functions
+// CSP_API void DummyFunction(const csp::Tests::TemplateClass<int>& obj);
+// CSP_API void DummyFunction(const csp::Tests::TemplateClass<csp::common::String>& obj);
+// CSP_API void DummyFunction(const csp::Tests::TemplateClass<SimpleClass*>& obj);
 
 } // namespace csp::Tests
