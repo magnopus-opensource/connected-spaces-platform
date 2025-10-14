a48f6e9a7ef90ddc7f570dd1ef58b89dfa30e666
MAG-mv <matthew.voisey@magnopus.com>
2025-10-14 19:04:57 +0100

    [NT-0] docs: fixing comments

--- Library/include/CSP/Common/ReplicatedValue.h
+++ Library/include/CSP/Common/ReplicatedValue.h
@@ -59,9 +59,10 @@ public:
 
     CSP_START_IGNORE
 
+    // These are great for certain scenarios such as serialization/deserialization, as we can avoid conditionals.
     // Internal templated setter.
     CSP_NO_EXPORT template <class T> inline void Set(const T& InValue) { Value = InValue; }
-    // Internal templated setter.
+    // Internal templated getter.
     CSP_NO_EXPORT template <class T> inline const T& Get() const { return std::get<T>(Value); }
 
     // Internal getter for variant.
