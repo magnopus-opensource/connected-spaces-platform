008b1fa8efc010b790c80e8ead570a1f155ea070
MAG-ChristopherAtkinson <Christopher.Atkinson@magnopus.global>
2025-11-21 11:47:13 +0000

    [NT-0] docs: updated inlined docuemntation for clarity around duration of refresh token

--- Library/include/CSP/Systems/Users/Authentication.h
+++ Library/include/CSP/Systems/Users/Authentication.h
@@ -108,7 +108,7 @@ public:
     /// be ignored.
     csp::common::String AccessTokenExpiryLength;
 
-    /// @brief The length of time for the refresh token to expire formatted as "HH:MM:SS", must be between "00:00:01" and "168:00:00"
+    /// @brief The length of time for the refresh token to expire formatted as "HH:MM:SS", must be between "00:00:01" and "168:00:00" (eq. 7 days)
     /// The default token expiry length is configured by MCS and defaults to 7 days. Value must be less than the default expiry length, or it will
     /// be ignored.
     csp::common::String RefreshTokenExpiryLength;
