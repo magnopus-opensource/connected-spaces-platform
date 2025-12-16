913b579380905b68b3fd65e2418fd44331fcb817
Elliot Morris <Elliot.Morris@magnopus.com>
2025-12-16 17:43:20 +0000

    [NT-0] chore: Establish hashing pattern
    Add std::hash specializations for our vector types, allowing them be stored in containers. The real reason for this however is so I can satisfy the IEquatable interface for C#.
    
    This starts the pattern for these, we're going to need to do this for a fair chunk of our types, any that are conceptually "value-types".
    
    We'll need one of these files for each of our 3 modules.
    The reason this is centralized is twofold:
    
    - std::hash specializations have to live in the std:: namespace so you don't get any information hiding by putting them along with the class anyhow
    - I need to ignore these from the current wrapper gen, but _not_ for the new one. The only way I can really do this at the moment is on a file-by-file basis.

---
+++ Library/include/CSP/Common/Hash.h
@@ -0,0 +1,55 @@
+/*
+ * Hash functions for CSP types in the common namespace
+ * Defined centrally, partly because it's a nice organization, but also partly due to wrapper gen constraints.
+ * Something having one of these is one of the best markers as to whether it's a "value-type" or not, albeit
+ * that term is nebulous. (Less so in other language runtimes though!)
+ *
+ * Try not to forget about these, they're very handy to have, and not having one means reference based languages
+ * like C# have to rely on reference equality which is really quite annoying, and leads to worse performance
+ * inside hashing containers.
+ */
+
+#include "CSP/CSPCommon.h"
+#include <CSP/Common/Vector.h>
+
+#include <functional>
+
+CSP_START_IGNORE
+namespace std
+{
+
+template <> struct hash<csp::common::Vector2>
+{
+    size_t operator()(const csp::common::Vector2& v) const noexcept
+    {
+        size_t h1 = std::hash<float> {}(v.X);
+        size_t h2 = std::hash<float> {}(v.Y);
+        return h1 ^ (h2 << 1);
+    }
+};
+
+template <> struct hash<csp::common::Vector3>
+{
+    size_t operator()(const csp::common::Vector3& v) const noexcept
+    {
+        size_t h1 = std::hash<float> {}(v.X);
+        size_t h2 = std::hash<float> {}(v.Y);
+        size_t h3 = std::hash<float> {}(v.Z);
+        return h1 ^ (h2 << 1) ^ (h3 << 2);
+    }
+};
+
+template <> struct hash<csp::common::Vector4>
+{
+    size_t operator()(const csp::common::Vector4& v) const noexcept
+    {
+        size_t h1 = std::hash<float> {}(v.X);
+        size_t h2 = std::hash<float> {}(v.Y);
+        size_t h3 = std::hash<float> {}(v.Z);
+        size_t h4 = std::hash<float> {}(v.W);
+        return h1 ^ (h2 << 1) ^ (h3 << 2) ^ (h4 << 3);
+    }
+};
+
+} // namespace std
+CSP_END_IGNORE
