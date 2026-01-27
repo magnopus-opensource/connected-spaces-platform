c843109a90f1e13f8a6f9a6e3da7d8767c8016f9
Elliot Morris <Elliot.Morris@magnopus.com>
2026-01-27 10:13:43 +0000

    Move Hash implementations that can be to cpp file
    Should have done this by default.

--- Library/include/CSP/Common/Hash.h
+++ Library/include/CSP/Common/Hash.h
@@ -31,58 +31,46 @@
 #include <CSP/Common/Array.h>
 #include <CSP/Common/List.h>
 #include <CSP/Common/Map.h>
-#include <CSP/Common/ReplicatedValue.h>
-#include <CSP/Common/Settings.h>
 #include <CSP/Common/String.h>
-#include <CSP/Common/Vector.h>
 
 #include <functional>
-#include <string_view>
+
+namespace csp::common
+{
+class Vector2;
+class Vector3;
+class Vector4;
+class ReplicatedValue;
+class ApplicationSettings;
+class SettingsCollection;
+}
 
 CSP_START_IGNORE
 namespace std
 {
 
-template <> struct hash<csp::common::Vector2>
+template <> struct CSP_API hash<csp::common::Vector2>
 {
-    size_t operator()(const csp::common::Vector2& v) const noexcept
-    {
-        size_t h1 = std::hash<float> {}(v.X);
-        size_t h2 = std::hash<float> {}(v.Y);
-        return h1 ^ (h2 << 1);
-    }
+    size_t operator()(const csp::common::Vector2& v) const noexcept;
 };
 
-template <> struct hash<csp::common::Vector3>
+template <> struct CSP_API hash<csp::common::Vector3>
 {
-    size_t operator()(const csp::common::Vector3& v) const noexcept
-    {
-        size_t h1 = std::hash<float> {}(v.X);
-        size_t h2 = std::hash<float> {}(v.Y);
-        size_t h3 = std::hash<float> {}(v.Z);
-        return h1 ^ (h2 << 1) ^ (h3 << 2);
-    }
+    size_t operator()(const csp::common::Vector3& v) const noexcept;
 };
 
-template <> struct hash<csp::common::Vector4>
+template <> struct CSP_API hash<csp::common::Vector4>
 {
-    size_t operator()(const csp::common::Vector4& v) const noexcept
-    {
-        size_t h1 = std::hash<float> {}(v.X);
-        size_t h2 = std::hash<float> {}(v.Y);
-        size_t h3 = std::hash<float> {}(v.Z);
-        size_t h4 = std::hash<float> {}(v.W);
-        return h1 ^ (h2 << 1) ^ (h3 << 2) ^ (h4 << 3);
-    }
+    size_t operator()(const csp::common::Vector4& v) const noexcept;
 };
 
-template <> struct hash<csp::common::String>
+template <> struct CSP_API hash<csp::common::String>
 {
-    size_t operator()(const csp::common::String& s) const noexcept { return std::hash<std::string_view> {}(s.c_str()); }
+    size_t operator()(const csp::common::String& s) const noexcept;
 };
 
-// These hashes arn't ideal. Even without getting super fancy you could vary the shift by element
-// to reduce collisions a lot, but they you'd need to handle wraparound.
+// These hashes aren't ideal. Even without getting super fancy you could vary the shift by element
+// to reduce collisions a lot, but then you'd need to handle wraparound.
 // Remember shifting by more than 64 (on x64 systems) is undefined behavior.
 template <typename T> struct hash<csp::common::Array<T>>
 {
@@ -123,39 +111,19 @@ template <typename TKey, typename TValue> struct hash<csp::common::Map<TKey, TVa
     }
 };
 
-template <> struct hash<csp::common::ReplicatedValue>
+template <> struct CSP_API hash<csp::common::ReplicatedValue>
 {
-    size_t operator()(const csp::common::ReplicatedValue& v) const noexcept
-    {
-        size_t TypeHash = std::hash<int> {}(static_cast<int>(v.GetReplicatedValueType()));
-        size_t ValueHash = std::visit(
-            [](const auto& val) -> size_t { return std::hash<std::decay_t<decltype(val)>> {}(val); }, v.GetValue());
-
-        return TypeHash ^ (ValueHash << 1);
-    }
+    size_t operator()(const csp::common::ReplicatedValue& v) const noexcept;
 };
 
-template <> struct hash<csp::common::ApplicationSettings>
+template <> struct CSP_API hash<csp::common::ApplicationSettings>
 {
-    size_t operator()(const csp::common::ApplicationSettings& s) const noexcept
-    {
-        size_t h1 = std::hash<csp::common::String> {}(s.ApplicationName);
-        size_t h2 = std::hash<csp::common::String> {}(s.Context);
-        size_t h3 = std::hash<bool> {}(s.AllowAnonymous);
-        size_t h4 = std::hash<csp::common::Map<csp::common::String, csp::common::String>> {}(s.Settings);
-        return h1 ^ (h2 << 1) ^ (h3 << 2) ^ (h4 << 3);
-    }
+    size_t operator()(const csp::common::ApplicationSettings& s) const noexcept;
 };
 
-template <> struct hash<csp::common::SettingsCollection>
+template <> struct CSP_API hash<csp::common::SettingsCollection>
 {
-    size_t operator()(const csp::common::SettingsCollection& s) const noexcept
-    {
-        size_t h1 = std::hash<csp::common::String> {}(s.UserId);
-        size_t h2 = std::hash<csp::common::String> {}(s.Context);
-        size_t h3 = std::hash<csp::common::Map<csp::common::String, csp::common::String>> {}(s.Settings);
-        return h1 ^ (h2 << 1) ^ (h3 << 2);
-    }
+    size_t operator()(const csp::common::SettingsCollection& s) const noexcept;
 };
 
 } // namespace std
