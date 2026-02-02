7a62ef2a13cc5558788c0f7d8d9c728a706a8aea
MAG-mv <matthew.voisey@magnopus.com>
2026-02-02 17:34:26 +0000

    [OB-5015] Leader election fixes (#898)
    * [OB-5019] fix: Scope leader is now registered instead of local client
    
    This was a bug which meant new clients always assumed they were the leader of the scope
    
    * [OB-5015] fix: Deregister remote script event with the event bus
    
    This was causing a crash which would keep the event around, event after the realtimeengine was destroyed
    
    * [OB-5015] test: Add script event test
    
    This test ensures remote script events are correctly fired to the scope leader. This ensures the both bugs in the PR are fixed, as it requires clients to have the valid scope leader, and a client the send and receive a remote script event
    
    * [OB-5015] docs: changelog
    
    * [OB-5015] test: add database write delay to fix spawned client sometimes not getting script source data

--- MultiplayerTestRunner/include/TestIdentifiers.h
+++ MultiplayerTestRunner/include/TestIdentifiers.h
@@ -54,12 +54,14 @@ enum class TestIdentifier
     CREATE_CONVERSATION, //"CreateConversation"
     EVENT_BUS_PING, //"EventBusPing"
     LEADER_ELECTION, //"LeaderElection"
-    LEADER_ELECTION_TICK // "LeaderElectionTick"
+    LEADER_ELECTION_TICK, // "LeaderElectionTick"
+    LEADER_ELECTION_EVENT // "LeaderElectionEvent"
 };
 
 inline const std::unordered_map<TestIdentifier, std::string> TestIdentifierStringMap { { TestIdentifier::CREATE_AVATAR, "CreateAvatar" },
     { TestIdentifier::CREATE_CONVERSATION, "CreateConversation" }, { TestIdentifier::EVENT_BUS_PING, "EventBusPing" },
-    { TestIdentifier::LEADER_ELECTION, "LeaderElection" }, { TestIdentifier::LEADER_ELECTION_TICK, "LeaderElectionTick" } };
+    { TestIdentifier::LEADER_ELECTION, "LeaderElection" }, { TestIdentifier::LEADER_ELECTION_TICK, "LeaderElectionTick" },
+    { TestIdentifier::LEADER_ELECTION_EVENT, "LeaderElectionEvent" } };
 
 /*
  * Use `TestIdentifierStringMap` to convert a string to a test identifier, if valid.
