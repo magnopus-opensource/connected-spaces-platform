c49ed351b22bc85166ae5a91ac5ea75460a50016
MAG-AdamThorn <adam.thorn@magnopus.com>
2026-02-18 09:25:24 +0000

    [OB-5141] fix: Remove unnecessary test friendships

--- Library/include/CSP/Systems/Analytics/AnalyticsSystem.h
+++ Library/include/CSP/Systems/Analytics/AnalyticsSystem.h
@@ -30,8 +30,6 @@ CSP_START_IGNORE
 class CSPEngine_AnalyticsSystemTests_QueueAnalyticsEventQueueSendRateTest_Test;
 class CSPEngine_AnalyticsSystemTests_QueueAnalyticsEventQueueSizeTest_Test;
 class CSPEngine_AnalyticsSystemTests_FlushAnalyticsEventsQueueTest_Test;
-class CSPEngine_AnalyticsSystemTests_ClearAnalyticsEventQueueTest_Test;
-class CSPEngine_AnalyticsSystemTests_FlushEmptyAnalyticsEventsQueueTest_Test;
 #endif
 CSP_END_IGNORE
 
@@ -71,8 +69,6 @@ class CSP_API AnalyticsSystem : public SystemBase
     friend class ::CSPEngine_AnalyticsSystemTests_QueueAnalyticsEventQueueSendRateTest_Test;
     friend class ::CSPEngine_AnalyticsSystemTests_QueueAnalyticsEventQueueSizeTest_Test;
     friend class ::CSPEngine_AnalyticsSystemTests_FlushAnalyticsEventsQueueTest_Test;
-    friend class ::CSPEngine_AnalyticsSystemTests_ClearAnalyticsEventQueueTest_Test;
-    friend class ::CSPEngine_AnalyticsSystemTests_FlushEmptyAnalyticsEventsQueueTest_Test;
 #endif
     /** @endcond */
     CSP_END_IGNORE
