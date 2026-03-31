64c18b3b9f357c49615acbe67c8e6eeb15e2df48
MAG-mv <matthew.voisey@magnopus.com>
2026-03-31 11:28:35 +0100

    [OF-1846] feat: initial commit

--- Library/src/Common/Web/POCOWebClient/POCOWebClient.cpp
+++ Library/src/Common/Web/POCOWebClient/POCOWebClient.cpp
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#ifndef CSP_WASM
+
 #include "POCOWebClient.h"
 
 #include "Debug/Logging.h"
@@ -576,3 +579,4 @@ void POCOWebClient::SetFileUploadContent(HttpPayload* Payload, Poco::Net::PartSo
 }
 
 } // namespace csp::web
+#endif
--- conanfile.py
+++ conanfile.py
@@ -17,32 +17,45 @@ class CSP(ConanFile):
         self.requires("rapidjson/cci.20250205")
         self.requires("fmt/12.1.0")
         self.requires("asyncplusplus/1.2")
-        self.requires("poco/1.14.2")
         self.requires("msgpack-cxx/7.0.0")
         self.requires("tinyspline/0.6.0")
         self.requires("glm/1.0.1")
         self.requires("gtest/1.11.0")
         self.requires("sole/1.0.4")
 
+        # We use the Emscripten WebSockets API for Emscripten
+        if self.settings.os != "Emscripten":
+            self.requires("poco/1.14.2")
+
     def configure(self):
         # Disable unnecessary Ppoco modules as this is a big library with dependecies.
         # Even with these removed, Poco increases our install step time by ~5 minutes.
-        self.options["poco"].enable_crypto = True
-        self.options["poco"].enable_netssl = True
-        self.options["poco"].enable_xml = True
-        self.options["poco"].enable_json = True
-        self.options["poco"].enable_util = True
-        self.options["poco"].enable_pdf = False
-        self.options["poco"].enable_zip = False
-        self.options["poco"].enable_data = False
-        self.options["poco"].enable_data_mysql = False
-        self.options["poco"].enable_data_postgresql = False
-        self.options["poco"].enable_data_sqlite = False
-        self.options["poco"].enable_mongodb = False
-        self.options["poco"].enable_redis = False
-        self.options["poco"].enable_prometheus = False
-        self.options["poco"].enable_jwt = False
-        self.options["poco"].enable_activerecord = False
+        if self.settings.os != "Emscripten":
+            self.options["poco"].enable_crypto = True
+            self.options["poco"].enable_netssl = True
+            self.options["poco"].enable_xml = True
+            self.options["poco"].enable_json = True
+            self.options["poco"].enable_util = True
+            self.options["poco"].enable_pdf = False
+            self.options["poco"].enable_zip = False
+            self.options["poco"].enable_data = False
+            self.options["poco"].enable_data_mysql = False
+            self.options["poco"].enable_data_postgresql = False
+            self.options["poco"].enable_data_sqlite = False
+            self.options["poco"].enable_mongodb = False
+            self.options["poco"].enable_redis = False
+            self.options["poco"].enable_prometheus = False
+            self.options["poco"].enable_jwt = False
+            self.options["poco"].enable_activerecord = False
+
+        # We need to turn of the c++ interface for tinyspline, as this is linked to its javscript (embind) interface
+        # which doesn't play well with our configuration for 2 reasons:
+        # - We use incompatible emscripten link flags with tinyspline
+        # - We use an incompatible emsdk, which causes compile errors with tinyspline
+        self.options["tinyspline"].cxx = False
+
+        # emscripten builds fail with boost. Disable it.
+        self.options["msgpack-cxx"].use_boost = False
 
     def generate(self):
         deps = CMakeDeps(self)
