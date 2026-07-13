2783b44685313a512b4b112da1b24a1555be6608
MAG-mv <matthew.voisey@magnopus.com>
2026-07-13 16:19:47 +0100

    [OF-1840] fix: Call npm package sript
    The package.json was not being generated without this script. This was causing th3e module not to be found

--- LegacyEmscriptenWrapper/GenerateLegacyEmscriptenPackage.sh
+++ LegacyEmscriptenWrapper/GenerateLegacyEmscriptenPackage.sh
@@ -34,6 +34,8 @@ CONFIG_OUTPUT_DIR="$OUTPUT_DIR/$CONFIG"
 WRAPPER_BUILD_DIR="./$BUILD_DIR/LegacyEmscriptenWrapper"
 TYPESCRIPT_OUTPUT_DIR="Tools/WrapperGenerator/Output/TypeScript"
 
+python ./teamcity/BuildNPMWebPackage.py --npm_publish_flag=False
+
 mkdir -p "$CONFIG_OUTPUT_DIR"
 
 # Copy specific Emscripten output files into the config folder
