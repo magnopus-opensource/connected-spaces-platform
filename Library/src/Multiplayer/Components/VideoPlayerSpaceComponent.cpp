magnopus-opensource/connected-spaces-platform

commit 289c271103b622e22de83c0f1413511e192200f9

[OF-1849] feat: Bind components created w/ Schemas into scripts automatically (#946)
* [OF-1865] chore: Make `Schema` type public and as such define in terms of interop types

This moves `csp::multiplayer::component::Schema` out of src and in to
public headers alongside `ComponentBase`. For simplicity of binding into
C# and TypeScript via the legacy wrapper gen, define these in term of
interop types.

Using these at the public API level is not a particularly smart choice
when considering ABI (particularly when consuming via C++ headers and
a prebuilt binary distribution of the library), but we do so liberally
already and you can't beat the convenience of these being value-like via
the wrapper generator.

This loosens the typing of the Ids, fixing them to uint64_t for the
component type id, and uint16_t for the property key, matching the
serialised wire representation we use for SignalR. This also removes the
type trait `IsPackableIdV`, which was introduced alongside the original
templated definitions of these types, as a way to ensure the given type
was correct at compile time. We could still statically assert here, but
it is a bit tautological. I think there is still something in trying to
statically express these constraints, but for now just get rid of it.

These are not yet made use of in any public methods, but will be soon.

* [OF-1865] chore: Add static `GetSchema()` method to all hardcoded components

This keeps the schema collocated with the existing logic, but provides
access to the schema definition for registration with the system in a
future commit.

* [OF-1865] feat: Expose engine-wide schema "registry" via `IRealtimeEngine`

This adds a getter to the `IRealtimeEngine` for a "registry" of component
schemas. For simplicity with the legacy wrapper generator, express this
as an interop `csp::common::Map` (vs rolling a new interface / type).

This is a const pointer vs a const reference in the API to hand own a

Library/src/Multiplayer/Components/VideoPlayerSpaceComponent.cpp
@@ -18,7 +18,7 @@
 #include "CSP/Multiplayer/OnlineRealtimeEngine.h"
 #include "CSP/Multiplayer/SpaceEntity.h"
 
-#include "Multiplayer/Component/Schema.h"
+#include "CSP/Multiplayer/ComponentSchema.h"
 #include "Multiplayer/Script/ComponentBinding/VideoPlayerSpaceComponentScriptInterface.h"
 
 #include <fmt/format.h>
@@ -32,111 +32,139 @@ constexpr const float DefaultVolume = 1.f;
 namespace csp::multiplayer
 {
 
-const auto Schema = ComponentBase::ComponentSchema {
-    ComponentType::VideoPlayer,
-    std::vector<ComponentBase::ComponentSchema::Property> {
+const auto Schema = ComponentSchema {
+    static_cast<ComponentSchema::TypeIdType>(ComponentType::VideoPlayer),
+    "VideoPlayer",
+    csp::common::Array<ComponentProperty> {
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::Name_DEPRECATED),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::Name_DEPRECATED),
+            "name",
             "",
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::VideoAssetId),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::VideoAssetId),
+            "videoAssetId",
             "",
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::VideoAssetURL),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::VideoAssetURL),
+            "videoAssetURL",
             "",
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::AssetCollectionId),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::AssetCollectionId),
+            "assetCollectionId",
             "",
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::Position),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::Position),
+            "position",
             csp::common::Vector3 { 0, 0, 0 },
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::Rotation),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::Rotation),
+            "rotation",
             csp::common::Vector4 { 0, 0, 0, 1 },
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::Scale),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::Scale),
+            "scale",
             csp::common::Vector3 { 1, 1, 1 },
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::IsStateShared),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::IsStateShared),
+            "isStateShared",
             false,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::IsAutoPlay),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::IsAutoPlay),
+            "isAutoPlay",
             false,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::IsLoopPlayback),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::IsLoopPlayback),
+            "isLoopPlayback",
             false,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::IsAutoResize),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::IsAutoResize),
+            "isAutoResize",
             false,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::PlaybackState),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::PlaybackState),
+            "playbackState",
             static_cast<int64_t>(0),
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::CurrentPlayheadPosition),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::CurrentPlayheadPosition),
+            "currentPlayheadPosition",
             0.0f,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::TimeSincePlay),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::TimeSincePlay),
+            "timeSincePlay",
             0.0f,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::AttenuationRadius),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::AttenuationRadius),
+            "attenuationRadius",
             DefaultAttenuationRadius,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::VideoPlayerSourceType),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::VideoPlayerSourceType),
+            "videoPlayerSourceType",
             static_cast<int64_t>(VideoPlayerSourceType::AssetSource),
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::StereoVideoType),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::StereoVideoType),
+            "stereoVideoType",
             static_cast<int64_t>(StereoVideoType::None),
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::IsStereoFlipped),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::IsStereoFlipped),
+            "isStereoFlipped",
             false,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::IsVisible),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::IsVisible),
+            "isVisible",
             true,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::IsARVisible),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::IsARVisible),
+            "isARVisible",
             true,
         },
         {
             static_cast<uint16_t>(VideoPlayerPropertyKeys::MeshComponentId),
+            {}, // not exposed to scripting
             static_cast<int64_t>(0),
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::IsEnabled),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::IsEnabled),
+            "isEnabled",
             true,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::IsVirtualVisible),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::IsVirtualVisible),
+            "isVirtualVisible",
             true,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::Volume),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::Volume),
+            {}, // not exposed to scripting via schema: we can't express value ranges (min, max) in schemas yet, so manually bind
             DefaultVolume,
         },
         {
-            static_cast<ComponentBase::PropertyKey>(VideoPlayerPropertyKeys::AudioType),
+            static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::AudioType),
+            "audioType",
             static_cast<int64_t>(AudioType::Spatial),
         },
     },
 };
+
+const ComponentSchema& VideoPlayerSpaceComponent::GetSchema() { return Schema; }
 
 VideoPlayerSpaceComponent::VideoPlayerSpaceComponent(csp::common::LogSystem* LogSystem, SpaceEntity* Parent)
     : ComponentBase(Schema, LogSystem, Parent)

Tests/src/PublicAPITests/ComponentTests/VideoPlayerComponentTests.cpp
@@ -215,6 +215,9 @@ CSP_PUBLIC_TEST(CSPEngine, VideoTests, VideoPlayerScriptInterfaceTest)
     EXPECT_EQ(VideoPlayerComponent->GetIsStereoFlipped(), false);
     EXPECT_EQ(VideoPlayerComponent->GetAudioType(), AudioType::Spatial);
     EXPECT_EQ(VideoPlayerComponent->GetVolume(), 1.f);
+    EXPECT_EQ(VideoPlayerComponent->GetVolume(), 1.f);
+    EXPECT_EQ(VideoPlayerComponent->GetIsAutoPlay(), false);
+    EXPECT_EQ(VideoPlayerComponent->GetAttenuationRadius(), 10.f);
 
     // Setup script
     const std::string VideoPlayerScriptText = R"xx(
@@ -242,7 +245,8 @@ CSP_PUBLIC_TEST(CSPEngine, VideoTests, VideoPlayerScriptInterfaceTest)
         video.isEnabled = false;
         video.audioType = 0;
         video.volume = 0.5;
-
+        video.isAutoPlay = true;
+        video.attenuationRadius = 22.0;
     )xx";
 
     ScriptComponent->SetScriptSource(VideoPlayerScriptText.c_str());
@@ -274,6 +278,32 @@ CSP_PUBLIC_TEST(CSPEngine, VideoTests, VideoPlayerScriptInterfaceTest)
     EXPECT_EQ(VideoPlayerComponent->GetIsEnabled(), false);
     EXPECT_EQ(VideoPlayerComponent->GetAudioType(), AudioType::Global);
     EXPECT_EQ(VideoPlayerComponent->GetVolume(), 0.5f);
+    EXPECT_EQ(VideoPlayerComponent->GetIsAutoPlay(), true);
+    EXPECT_EQ(VideoPlayerComponent->GetAttenuationRadius(), 22.f);
+
+    // volume input range validation
+    {
+        const auto ExpectedVolume = VideoPlayerComponent->GetVolume();
+        CreatedObject->GetScript().SetScriptSource(R"(
+            const video = ThisEntity.getVideoPlayerComponents()[0];
+            video.volume = 1.75;
+        )");
+
+        CreatedObject->GetScript().Invoke();
+        RealtimeEngine->ProcessPendingEntityOperations();
+
+        EXPECT_EQ(VideoPlayerComponent->GetVolume(), ExpectedVolume);
+    
+        CreatedObject->GetScript().SetScriptSource(R"(
+            const video = ThisEntity.getVideoPlayerComponents()[0];
+            video.volume = -2.75;
+        )");
+
+        CreatedObject->GetScript().Invoke();
+        RealtimeEngine->ProcessPendingEntityOperations();
+
+        EXPECT_EQ(VideoPlayerComponent->GetVolume(), ExpectedVolume);
+    }
 
     auto [ExitSpaceResult] = AWAIT_PRE(SpaceSystem, ExitSpace, RequestPredicate);
 

commit 5f73444bbf2fdfde0fd3e045427b2d5ebe158331

[OB-5754] fix: Expose base component name vs deprecated name to scripting for VideoPlayer
The existing bindings for `ComponentBase` expose the newer conventional
name (`GetComponentName`/`SetComponentName`) as `name` in the bindings,
which means all components will naturally pick that up. However, it has
always been the case that some components were overriding that by
defining their own "name" property in the script bindings, which takes
precedence in the js prototype chain. The same happens here if a schema
specifies an explicit `name` property.

This regressed in 289c271103b622e22de83c0f1413511e192200f9. I had missed
when making those changes that the video player component had specific
custom handling for exposing the name (introduced as part of the changes
landed in a40ea2b5063e956dee4ab6aaceaedaa32c60804e, specifically the
`DEFINE_SCRIPT_PROPERTY_STRING_ADAPTNAME` macro [here](https://github.com/magnopus-opensource/connected-spaces-platform/pull/591/changes/387507c66b2ff19cd033a36bc967433a87a6721e#diff-79b320b89915f938f7d35a76573e6f0368cddbf03dac43aeeada2d2062048470R30-R32).

I had been porting over the existing bindings as they were specified in
`EntityScriptBinding`, which means that `ExternalLink`, `FiducialMarker`,
and `Image` components are still exposing the deprecated name properties
that each of those components store. Those are still left as-is here,
only addressing the specific regression (technically switching those
would be a different regression, i.e. going from the older name format
to the new one, but likely a rather benign change).

With the schema as it is, we stop this by not giving the deprecated
property an explicit name. This will be better expressed once the
changes have landed that extend the schema to, amongst other things,
explicitly not expose properties to scripting.

Library/src/Multiplayer/Components/VideoPlayerSpaceComponent.cpp
@@ -38,7 +38,7 @@ const auto Schema = ComponentSchema {
     csp::common::Array<ComponentProperty> {
         {
             static_cast<ComponentProperty::KeyType>(VideoPlayerPropertyKeys::Name_DEPRECATED),
-            "name",
+            {}, // not exposed to scripting
             "",
         },
         {

Tests/src/PublicAPITests/ComponentTests/VideoPlayerComponentTests.cpp
@@ -194,6 +194,7 @@ CSP_PUBLIC_TEST(CSPEngine, VideoTests, VideoPlayerScriptInterfaceTest)
     CreatedObject->QueueUpdate();
     RealtimeEngine->ProcessPendingEntityOperations();
 
+    EXPECT_EQ(VideoPlayerComponent->GetComponentName(), "");
     EXPECT_EQ(VideoPlayerComponent->GetPosition(), csp::common::Vector3::Zero());
     EXPECT_EQ(VideoPlayerComponent->GetScale(), csp::common::Vector3::One());
     EXPECT_EQ(VideoPlayerComponent->GetRotation(), csp::common::Vector4::Identity());
@@ -224,6 +225,7 @@ CSP_PUBLIC_TEST(CSPEngine, VideoTests, VideoPlayerScriptInterfaceTest)
 
 		var video = ThisEntity.getVideoPlayerComponents()[0];
 
+        video.name = "VideoPlayerTest";
         video.position = [1, 1, 1];
         video.scale = [2, 2, 2];
 		video.rotation = [1, 1, 1, 1];
@@ -257,6 +259,7 @@ CSP_PUBLIC_TEST(CSPEngine, VideoTests, VideoPlayerScriptInterfaceTest)
     const bool ScriptHasErrors = CreatedObject->GetScript().HasError();
     EXPECT_FALSE(ScriptHasErrors);
 
+    EXPECT_EQ(VideoPlayerComponent->GetComponentName(), "VideoPlayerTest");
     EXPECT_EQ(VideoPlayerComponent->GetPosition(), csp::common::Vector3::One());
     EXPECT_EQ(VideoPlayerComponent->GetScale(), csp::common::Vector3(2, 2, 2));
     EXPECT_EQ(VideoPlayerComponent->GetRotation(), csp::common::Vector4::One());